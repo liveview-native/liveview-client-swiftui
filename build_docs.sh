2dc198b01acab58b7aad26f6f6beca4c36b4d1f5
Shadowfacts <me@shadowfacts.net>
2022-08-11 16:16:35 -0400

    Update doc build script

--- build_docs.sh
+++ build_docs.sh
@@ -1,7 +1,30 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# build doccarchive
 mkdir -p docc_build
 xcodebuild docbuild -scheme PhoenixLiveViewNative -destination generic/platform=iOS -derivedDataPath docc_build
 
-xcrun docc process-archive transform-for-static-hosting docc_build/Build/Products/Debug-iphoneos/PhoenixLiveViewNative.doccarchive --output-path docc_out --hosting-base-path liveview-client-swiftui
+# create worktree
+if [ ! -d docs ]; then
+	git worktree add docs docs
+fi
+
+# remove any stale files
+rm -rf docs/*
+
+xcrun docc process-archive transform-for-static-hosting docc_build/Build/Products/Debug-iphoneos/PhoenixLiveViewNative.doccarchive --output-path docs --hosting-base-path /liveview-client-swiftui
+
+# add index page to root with redirect to package docs
+cat > docs/index.html << EOF
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="refresh" content="0; url=/liveview-client-swiftui/documentation/phoenixliveviewnative">
+</head>
+<body>
+</body>
+</html>
+EOF
+
+echo -e "\x1B[1mDocs updated, commit the result in the docs/ directory.\x1B[0m"
