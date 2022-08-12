d6dcb2f185458a5122b980126f398d2b3cf77063
Shadowfacts <me@shadowfacts.net>
2022-08-12 15:10:56 -0400

    Tutorial: Fix incorrect URL
    Closes #15

--- Sources/PhoenixLiveViewNative/PhoenixLiveViewNative.docc/Resources/01-02-07-liveview.swift
+++ Sources/PhoenixLiveViewNative/PhoenixLiveViewNative.docc/Resources/01-02-07-liveview.swift
@@ -2,7 +2,7 @@ import SwiftUI
 import PhoenixLiveViewNative
 
 struct ContentView: View {
-    @State var coordinator = LiveViewCoordinator(URL(string: "http://localhost:4000/")!)
+    @State var coordinator = LiveViewCoordinator(URL(string: "http://localhost:4000/cats")!)
 
     var body: some View {
         LiveView(coordinator: coordinator)
