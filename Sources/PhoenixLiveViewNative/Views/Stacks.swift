d4f2e55c769c9b623a86163b3b968de1ae712b23
Shadowfacts <me@shadowfacts.net>
2022-08-24 14:54:31 -0400

    Add <vstack> alignment attribute

--- Sources/PhoenixLiveViewNative/Views/Stacks.swift
+++ Sources/PhoenixLiveViewNative/Views/Stacks.swift
@@ -22,11 +22,27 @@ public struct PhxHStack<R: CustomRegistry>: View {
 
 /// `<vstack>`, lays out children in a vertical line
 public struct PhxVStack<R: CustomRegistry>: View {
-    var element: Element
-    var context: LiveContext<R>
+    private let element: Element
+    private let context: LiveContext<R>
+    private let alignment: HorizontalAlignment
+    
+    init(element: Element, context: LiveContext<R>) {
+        self.element = element
+        self.context = context
+        switch element.attrIfPresent("alignment") {
+        case nil, "center":
+            self.alignment = .center
+        case "leading":
+            self.alignment = .leading
+        case "trailing":
+            self.alignment = .trailing
+        default:
+            fatalError("Invalid value '\(try! element.attr("alignment"))' for alignment attribute of <vstack>")
+        }
+    }
 
     public var body: some View {
-        VStack {
+        VStack(alignment: alignment) {
             context.buildChildren(of: element)
         }
     }
