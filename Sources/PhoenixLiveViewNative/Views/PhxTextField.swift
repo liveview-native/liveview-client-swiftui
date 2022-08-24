93f199a0dbb01f076136235a208128fcb0f9d61d
Shadowfacts <me@shadowfacts.net>
2022-08-24 11:41:05 -0400

    Add more <textfield> attributes

--- Sources/PhoenixLiveViewNative/Views/PhxTextField.swift
+++ Sources/PhoenixLiveViewNative/Views/PhxTextField.swift
@@ -12,9 +12,7 @@ import SwiftSoup
 public struct PhxTextField<R: CustomRegistry>: View {
     private let label: String
     private let name: String
-    private let placeholder: String?
-    private let borderStyle: UITextField.BorderStyle
-    private let clearButtonMode: UITextField.ViewMode
+    private let config: TextFieldConfiguration
     private let formModel: FormModel
     @FormState private var value: String?
     @State private var becomeFirstResponder = false
@@ -24,31 +22,13 @@ public struct PhxTextField<R: CustomRegistry>: View {
         // throwing: .attr only throws if the given attribute name is empty
         self.label = try! element.attr("label")
         self.name = try! element.attr("name")
-        self.placeholder = element.attrIfPresent("placeholder")
-        switch element.attrIfPresent("border-style") {
-        case nil, "rounded":
-            borderStyle = .roundedRect
-        case "none":
-            borderStyle = .none
-        default:
-            fatalError("Invalid value '\(element.attrIfPresent("border-style")!)' for border-style attribute on <textfield>")
-        }
-        switch element.attrIfPresent("clear-button") {
-        case nil, "never":
-            clearButtonMode = .never
-        case "always":
-            clearButtonMode = .always
-        case "while-editing":
-            clearButtonMode = .whileEditing
-        default:
-            fatalError("Invalid value '\(element.attrIfPresent("clear-button")!)' for clear-button attribute on <textfield>")
-        }
+        self.config = TextFieldConfiguration(element: element)
         precondition(!name.isEmpty, "<textfield> must have name")
         self.formModel = context.formModel!
     }
     
     public var body: some View {
-        return PhxWrappedTextField(formModel: formModel, name: name, placeholder: placeholder, borderStyle: borderStyle, clearButtonMode: clearButtonMode, value: $value, becomeFirstResponder: $becomeFirstResponder)
+        return PhxWrappedTextField(formModel: formModel, name: name, config: config, value: $value, becomeFirstResponder: $becomeFirstResponder)
             .frame(height: 44)
             .onAppear {
                 // If the DOM changes, the text field can get re-created and destroyed even though
@@ -64,25 +44,20 @@ fileprivate struct PhxWrappedTextField: UIViewRepresentable {
     typealias UIViewType = UITextField
     private let formModel: FormModel
     private let name: String
-    private let placeholder: String?
-    private let borderStyle: UITextField.BorderStyle
-    private let clearButtonMode: UITextField.ViewMode
+    private let config: TextFieldConfiguration
     @Binding private var value: String?
     @Binding private var becomeFirstResponder: Bool
     
-    init(formModel: FormModel, name: String, placeholder: String?, borderStyle: UITextField.BorderStyle, clearButtonMode: UITextField.ViewMode, value: Binding<String?>, becomeFirstResponder: Binding<Bool>) {
+    init(formModel: FormModel, name: String, config: TextFieldConfiguration, value: Binding<String?>, becomeFirstResponder: Binding<Bool>) {
         self.formModel = formModel
         self.name = name
-        self.placeholder = placeholder
-        self.borderStyle = borderStyle
-        self.clearButtonMode = clearButtonMode
+        self.config = config
         self._value = value
         self._becomeFirstResponder = becomeFirstResponder
     }
     
     func makeUIView(context: Context) -> UITextField {
         let field = UITextField()
-        field.borderStyle = borderStyle
         field.backgroundColor = .clear
         field.addTarget(context.coordinator, action: #selector(Coordinator.editingChanged), for: .editingChanged)
         field.delegate = context.coordinator
@@ -93,8 +68,7 @@ fileprivate struct PhxWrappedTextField: UIViewRepresentable {
     
     func updateUIView(_ uiView: UITextField, context: Context) {
         uiView.text = value
-        uiView.placeholder = placeholder
-        uiView.clearButtonMode = clearButtonMode
+        config.apply(to: uiView)
         if becomeFirstResponder {
             DispatchQueue.main.async {
                 // becoming first responder immediately breaks some internal autocorrect thing, so we wait until the next runloop iteration
@@ -137,5 +111,93 @@ fileprivate struct PhxWrappedTextField: UIViewRepresentable {
         func textFieldDidEndEditing(_ textField: UITextField) {
             formModel.focusedFieldName = nil
         }
+    }
+}
+
+private struct TextFieldConfiguration {
+    let placeholder: String?
+    let borderStyle: UITextField.BorderStyle
+    let clearButtonMode: UITextField.ViewMode
+    let autocorrectionType: UITextAutocorrectionType
+    let autocapitalizationType: UITextAutocapitalizationType
+    let keyboardType: UIKeyboardType
+    
+    init(element: Element) {
+        self.placeholder = element.attrIfPresent("placeholder")
+        switch element.attrIfPresent("border-style") {
+        case nil, "rounded":
+            borderStyle = .roundedRect
+        case "none":
+            borderStyle = .none
+        default:
+            fatalError("Invalid value '\(element.attrIfPresent("border-style")!)' for border-style attribute on <textfield>")
+        }
+        switch element.attrIfPresent("clear-button") {
+        case nil, "never":
+            clearButtonMode = .never
+        case "always":
+            clearButtonMode = .always
+        case "while-editing":
+            clearButtonMode = .whileEditing
+        default:
+            fatalError("Invalid value '\(element.attrIfPresent("clear-button")!)' for clear-button attribute on <textfield>")
+        }
+        switch element.attrIfPresent("autocorrection") {
+        case nil, "default":
+            autocorrectionType = .default
+        case "no":
+            autocorrectionType = .no
+        case "yes":
+            autocorrectionType = .yes
+        default:
+            fatalError("Invalid value '\(element.attrIfPresent("autocorrection")!)' for autocorrection attribute on <textfield>")
+        }
+        switch element.attrIfPresent("autocapitalization") {
+        case nil, "sentences":
+            autocapitalizationType = .sentences
+        case "none":
+            autocapitalizationType = .none
+        case "words":
+            autocapitalizationType = .words
+        case "all-characters":
+            autocapitalizationType = .allCharacters
+        default:
+            fatalError("Invalid value '\(element.attrIfPresent("autocapitalization")!)' for autocapitalization attribute on <textfield>")
+        }
+        switch element.attrIfPresent("keyboard") {
+        case nil, "default":
+            keyboardType = .default
+        case "ascii-capable":
+            keyboardType = .asciiCapable
+        case "numbers-and-punctuation":
+            keyboardType = .numbersAndPunctuation
+        case "url":
+            keyboardType = .URL
+        case "number-pad":
+            keyboardType = .numberPad
+        case "phone-pad":
+            keyboardType = .phonePad
+        case "name-phone-pad":
+            keyboardType = .namePhonePad
+        case "email":
+            keyboardType = .emailAddress
+        case "decimal":
+            keyboardType = .decimalPad
+        case "twitter":
+            keyboardType = .twitter
+        case "ascii-capable-number-pad":
+            keyboardType = .asciiCapableNumberPad
+        default:
+            fatalError("Invalid value '\(element.attrIfPresent("keyboard")!)' for keyboard attribute on <textfield>")
+        }
+    }
+    
+    func apply(to view: UITextField) {
+        view.borderStyle = borderStyle
+        view.placeholder = placeholder
+        view.clearButtonMode = clearButtonMode
+        view.autocorrectionType = autocorrectionType
+        view.autocapitalizationType = autocapitalizationType
+        view.keyboardType = keyboardType
     }
 }
