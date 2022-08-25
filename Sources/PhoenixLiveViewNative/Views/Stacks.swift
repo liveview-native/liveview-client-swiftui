0af2bc7a649cd48d82c20b104a5077e278d81d9f
Shadowfacts <me@shadowfacts.net>
2022-08-25 14:54:54 -0400

    Add <hstack> alignment

--- Sources/PhoenixLiveViewNative/Views/Stacks.swift
+++ Sources/PhoenixLiveViewNative/Views/Stacks.swift
@@ -10,11 +10,27 @@ import SwiftSoup
 
 /// `<hstack>`, lays out children in a horizontal line.
 public struct PhxHStack<R: CustomRegistry>: View {
-    var element: Element
-    var context: LiveContext<R>
+    private let element: Element
+    private let context: LiveContext<R>
+    private let alignment: VerticalAlignment
     
+    init(element: Element, context: LiveContext<R>) {
+        self.element = element
+        self.context = context
+        switch element.attrIfPresent("alignment") {
+        case nil, "center":
+            self.alignment = .center
+        case "top":
+            self.alignment = .top
+        case "bottom":
+            self.alignment = .bottom
+        default:
+            fatalError("Invalid value '\(try! element.attr("alignment"))' for alignment attribute of <hstack>")
+        }
+    }
+
     public var body: some View {
-        HStack {
+        HStack(alignment: alignment) {
             context.buildChildren(of: element)
         }
     }
