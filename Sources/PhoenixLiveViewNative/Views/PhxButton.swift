d9c46012fa9aea01e0a18449bb5b9c51cd6ff49d
Shadowfacts <me@shadowfacts.net>
2022-08-31 14:04:40 -0400

    Add button styles

--- Sources/PhoenixLiveViewNative/Views/PhxButton.swift
+++ Sources/PhoenixLiveViewNative/Views/PhxButton.swift
@@ -13,6 +13,7 @@ public struct PhxButton<R: CustomRegistry>: View {
     private let element: Element
     private let context: LiveContext<R>
     private let clickEvent: String?
+    private let buttonStyle: PhxButtonStyle
     private let disabled: Bool
     
     init(element: Element, context: LiveContext<R>) {
@@ -20,13 +21,19 @@ public struct PhxButton<R: CustomRegistry>: View {
         self.context = context
         self.clickEvent = element.attrIfPresent("phx-click")
         self.disabled = element.hasAttr("disabled")
+        if let s = element.attrIfPresent("button-style"),
+           let style = PhxButtonStyle(rawValue: s) {
+            self.buttonStyle = style
+        } else {
+            self.buttonStyle = .automatic
+        }
     }
     
     public var body: some View {
         Button(action: self.handleClick) {
             context.buildChildren(of: element)
         }
-        .buttonStyle(.plain)
+        .phxButtonStyle(buttonStyle)
         .disabled(disabled)
     }
     
@@ -39,4 +46,30 @@ public struct PhxButton<R: CustomRegistry>: View {
         }
     }
     
+}
+
+fileprivate enum PhxButtonStyle: String {
+    case automatic
+    case bordered
+    case borderedProminent = "bordered-prominent"
+    case borderless
+    case plain
+}
+
+fileprivate extension View {
+    @ViewBuilder
+    func phxButtonStyle(_ style: PhxButtonStyle) -> some View {
+        switch style {
+        case .automatic:
+            self.buttonStyle(.automatic)
+        case .bordered:
+            self.buttonStyle(.bordered)
+        case .borderedProminent:
+            self.buttonStyle(.borderedProminent)
+        case .borderless:
+            self.buttonStyle(.borderless)
+        case .plain:
+            self.buttonStyle(.plain)
+        }
+    }
 }
