562c935a1c80c99f3d30c00d34b1cf4a9c2dfc08
Carson Katri <Carson.katri@gmail.com>
2023-01-18 13:56:27 -0500

    Simplify NavigationLink to use only 'href'

--- Sources/LiveViewNative/Views/Layout Containers/Presentation Containers/NavigationLink.swift
+++ Sources/LiveViewNative/Views/Layout Containers/Presentation Containers/NavigationLink.swift
@@ -21,56 +21,18 @@ struct NavigationLink<R: CustomRegistry>: View {
     
     @ViewBuilder
     public var body: some View {
-        if let linkOpts = LinkOptions(element: element),
-           context.coordinator.session.config.navigationMode.supportsLinkState(linkOpts.state),
-           let url = linkOpts.url(in: context)
-        {
-            SwiftUI.NavigationLink(value: LiveNavigationEntry(url: url, coordinator: context.coordinator)) {
+        if let href = element.attributeValue(for: "href").flatMap({
+            URL(string: $0, relativeTo: context.coordinator.url)?.appending(path: "").absoluteURL
+        }) {
+            SwiftUI.NavigationLink(
+                value: LiveNavigationEntry(
+                    url: href,
+                    coordinator: context.coordinator
+                )
+            ) {
                 context.buildChildren(of: element)
             }
             .disabled(element.attribute(named: "disabled") != nil)
-        } else {
-            // if there are no link options, or the coordinator doesn't support the required navigation, we don't show anything
-        }
-    }
-}
-
-struct LinkOptions {
-    let kind: LinkKind
-    let state: LiveRedirect.Kind
-    let href: String
-    
-    init?(element: ElementNode) {
-        guard let kindStr = element.attributeValue(for: "data-phx-link"),
-              let kind = LinkKind(rawValue: kindStr),
-              let stateStr = element.attributeValue(for: "data-phx-link-state"),
-              let state = LiveRedirect.Kind(rawValue: stateStr) else {
-            return nil
-        }
-        self.kind = kind
-        self.state = state
-        self.href = element.attributeValue(for: "data-phx-href")!
-    }
-    
-    @MainActor
-    func url<R: CustomRegistry>(in context: LiveContext<R>) -> URL? {
-        .init(string: href, relativeTo: context.coordinator.url)?.appending(path: "/").absoluteURL
-    }
-}
-
-enum LinkKind: String {
-    case redirect
-}
-
-extension LiveSessionConfiguration.NavigationMode {
-    func supportsLinkState(_ state: LiveRedirect.Kind) -> Bool {
-        switch self {
-        case .disabled:
-            return false
-        case .replaceOnly:
-            return state == .replace
-        case .enabled, .splitView:
-            return true
         }
     }
 }
