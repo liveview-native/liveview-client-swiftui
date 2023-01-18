951790d0e6ca8d39dc99653d3723f4386c57bceb
Shadowfacts <me@shadowfacts.net>
2023-01-18 11:15:14 -0500

    Add Form

---
+++ Sources/LiveViewNative/Views/Layout Containers/Forms/Form.swift
@@ -0,0 +1,46 @@
+//
+//  Form.swift
+//  LiveViewNative
+//
+//  Created by Shadowfacts on 1/18/23.
+//
+
+import SwiftUI
+
+struct Form<R: CustomRegistry>: View {
+    @ObservedElement private var element
+    private let context: LiveContext<R>
+    
+    init(context: LiveContext<R>) {
+        self.context = context
+    }
+    
+    var body: some View {
+        SwiftUI.Form {
+            context.buildChildren(of: element)
+        }
+        .applyFormStyle(formStyle)
+    }
+    
+    private var formStyle: FormStyle {
+        element.attributeValue(for: "form-style").flatMap(FormStyle.init) ?? .automatic
+    }
+}
+
+private enum FormStyle: String {
+    case automatic, columns, grouped
+}
+
+private extension View {
+    @ViewBuilder
+    func applyFormStyle(_ style: FormStyle) -> some View {
+        switch style {
+        case .automatic:
+            self.formStyle(.automatic)
+        case .columns:
+            self.formStyle(.columns)
+        case .grouped:
+            self.formStyle(.grouped)
+        }
+    }
+}
