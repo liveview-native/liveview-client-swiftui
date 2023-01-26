1632f728441f5ff2ba05c7127bc35d09193b186d
Carson Katri <Carson.katri@gmail.com>
2023-01-26 11:54:16 -0500

    Add ButtonTests

--- Tests/RenderingTests/ButtonTests.swift
+++ Tests/RenderingTests/ButtonTests.swift
@@ -1,8 +1,60 @@
 //
-//  File.swift
+//  ButtonTests.swift
 //  
 //
-//  Created by Carson.Katri on 1/26/23.
+//  Created by Carson Katri on 1/26/23.
 //
 
-import Foundation
+import XCTest
+import SwiftUI
+@testable import LiveViewNative
+
+@MainActor
+final class ButtonTests: XCTestCase {
+    func testButtonSimple() throws {
+        try assertMatch(#"<button>Click Me</button>"#) {
+            Button("Click Me") {}
+        }
+    }
+    func testButtonComplexBody() throws {
+        try assertMatch(
+            #"""
+            <button>
+                <h-stack>
+                    <image system-name="circle.fill" />
+                    <text>Tap Here</text>
+                </h-stack>
+            </button>
+            """#
+        ) {
+            Button(action: {}) {
+                HStack {
+                    Image(systemName: "circle.fill")
+                    Text("Tap Here")
+                }
+            }
+        }
+    }
+    func testButtonStyles() throws {
+        try assertMatch(#"<button button-style="automatic">Click Me</button>"#) {
+            Button("Click Me") {}
+                .buttonStyle(.automatic)
+        }
+        try assertMatch(#"<button button-style="bordered">Click Me</button>"#) {
+            Button("Click Me") {}
+                .buttonStyle(.bordered)
+        }
+        try assertMatch(#"<button button-style="bordered-prominent">Click Me</button>"#) {
+            Button("Click Me") {}
+                .buttonStyle(.borderedProminent)
+        }
+        try assertMatch(#"<button button-style="borderless">Click Me</button>"#) {
+            Button("Click Me") {}
+                .buttonStyle(.borderless)
+        }
+        try assertMatch(#"<button button-style="plain">Click Me</button>"#) {
+            Button("Click Me") {}
+                .buttonStyle(.plain)
+        }
+    }
+}
