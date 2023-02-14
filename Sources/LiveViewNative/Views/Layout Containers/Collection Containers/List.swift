798457c38447e91cadf686a1694ef7c94669e46f
Carson Katri <Carson.katri@gmail.com>
2023-02-14 11:24:33 -0500

    Support single and multi-value LiveBinding selections

--- Sources/LiveViewNative/Views/Layout Containers/Collection Containers/List.swift
+++ Sources/LiveViewNative/Views/Layout Containers/Collection Containers/List.swift
@@ -11,16 +11,87 @@ struct List<R: CustomRegistry>: View {
     @ObservedElement private var element: ElementNode
     private let context: LiveContext<R>
     
+    /// Selection of either a single value or set of values.
+    enum Selection: Codable {
+        case single(String?)
+        case multiple(Set<String>)
+        
+        var single: String? {
+            get {
+                guard case let .single(selection) = self else { fatalError() }
+                return selection
+            }
+            set {
+                self = .single(newValue)
+            }
+        }
+        
+        var multiple: Set<String> {
+            get {
+                guard case let .multiple(selection) = self else { fatalError() }
+                return selection
+            }
+            set {
+                self = .multiple(newValue)
+            }
+        }
+        
+        init(from decoder: Decoder) throws {
+            let container = try decoder.singleValueContainer()
+            if container.decodeNil() {
+                self = .single(nil)
+            } else if let item = try? container.decode(String.self) {
+                self = .single(item)
+            } else {
+                self = .multiple(try container.decode(Set<String>.self))
+            }
+        }
+        
+        func encode(to encoder: Encoder) throws {
+            var container = encoder.singleValueContainer()
+            switch self {
+            case .single(let selection):
+                try container.encode(selection)
+            case .multiple(let selection):
+                try container.encode(selection)
+            }
+        }
+    }
+    
+    @LiveBinding(attribute: "selection") private var selection = Selection.multiple([])
+    
     init(element: ElementNode, context: LiveContext<R>) {
         self.context = context
     }
     
     public var body: some View {
+        list
+            .applyListStyle(element.attributeValue(for: "list-style").flatMap(ListStyle.init) ?? .automatic)
+    }
+    
+    @ViewBuilder
+    private var list: some View {
+        #if os(watchOS)
         SwiftUI.List {
-            forEach(nodes: element.children(), context: context)
-                .onDelete(perform: onDeleteHandler)
+            content
         }
-        .applyListStyle(element.attributeValue(for: "list-style").flatMap(ListStyle.init) ?? .automatic)
+        #else
+        switch selection {
+        case .single:
+            SwiftUI.List(selection: $selection.single) {
+                content
+            }
+        case .multiple:
+            SwiftUI.List(selection: $selection.multiple) {
+                content
+            }
+        }
+        #endif
+    }
+    
+    private var content: some View {
+        forEach(nodes: element.children(), context: context)
+            .onDelete(perform: onDeleteHandler)
     }
     
     private var onDeleteHandler: ((IndexSet) -> Void)? {
@@ -54,7 +125,7 @@ fileprivate enum ListStyle: String {
 #endif
 }
 
-private extension SwiftUI.List {
+private extension View {
     @ViewBuilder
     func applyListStyle(_ style: ListStyle) -> some View {
         switch style {
