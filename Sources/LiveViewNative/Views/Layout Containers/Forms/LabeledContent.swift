bcb28ae2dfd25efc817d4f2324d184ba57d6b5cb
Carson Katri <Carson.katri@gmail.com>
2023-02-23 09:45:01 -0500

    Add LabeledContent

---
+++ Sources/LiveViewNative/Views/Layout Containers/Forms/LabeledContent.swift
@@ -0,0 +1,39 @@
+//
+//  LabeledContent.swift
+//  
+//
+//  Created by Carson.Katri on 2/23/23.
+//
+
+import SwiftUI
+
+struct LabeledContent<R: CustomRegistry>: View {
+    @ObservedElement private var element
+    private let context: LiveContext<R>
+    
+    init(context: LiveContext<R>) {
+        self.context = context
+    }
+    
+    var body: some View {
+        SwiftUI.LabeledContent {
+            context.buildChildren(of: element, withTagName: "content", namespace: "labeled-content", includeDefaultSlot: true)
+        } label: {
+            context.buildChildren(of: element, withTagName: "label", namespace: "labeled-content")
+        }
+        .applyLabeledContentStyle(element.attributeValue(for: "labeled-content-style").flatMap(LabeledContentStyle.init) ?? .automatic)
+    }
+}
+
+private enum LabeledContentStyle: String {
+    case automatic
+}
+
+private extension View {
+    @ViewBuilder
+    func applyLabeledContentStyle(_ style: LabeledContentStyle) -> some View {
+        switch style {
+        case .automatic: self.labeledContentStyle(.automatic)
+        }
+    }
+}
