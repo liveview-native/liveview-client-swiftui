c04e6f984bbf7cf99ed5864a3854955b20024c53
Carson Katri <Carson.katri@gmail.com>
2023-03-16 14:24:07 -0400

    Document Link

--- Sources/LiveViewNative/Views/Controls and Indicators/Links/Link.swift
+++ Sources/LiveViewNative/Views/Controls and Indicators/Links/Link.swift
@@ -7,10 +7,23 @@
 
 import SwiftUI
 
+/// Opens a URL when tapped.
+///
+/// Provide a ``destination`` and label content to create a ``Link``.
+///
+/// ```html
+/// <Link destination="https://native.live">
+///     Go to <Text font="body" font-weight="bold">LiveView Native</Text>
+/// </Link>
+/// ```
+///
+/// ## Attributes
+/// * ``destination``
 struct Link<R: RootRegistry>: View {
     @ObservedElement private var element: ElementNode
     let context: LiveContext<R>
     
+    /// A valid URL to open when tapped.
     @Attribute("destination", transform: { $0?.value.flatMap(URL.init(string:)) }) private var destination: URL?
     
     init(element: ElementNode, context: LiveContext<R>) {
