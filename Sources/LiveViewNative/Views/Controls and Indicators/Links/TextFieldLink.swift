c10890a44a3c5ec1d1c478b9d0d7e5776f73fbf1
Carson Katri <Carson.katri@gmail.com>
2023-03-16 15:25:50 -0400

    Document TextFieldLink

--- Sources/LiveViewNative/Views/Controls and Indicators/Links/TextFieldLink.swift
+++ Sources/LiveViewNative/Views/Controls and Indicators/Links/TextFieldLink.swift
@@ -5,26 +5,46 @@
 //  Created by Carson Katri on 3/2/23.
 //
 
-#if os(watchOS)
 import SwiftUI
 
+/// A form element that requests text input on Apple Watch.
+///
+/// This element displays a button that, when tapped, opens a dialogue for inputting text.
+///
+/// ```html
+/// <TextFieldLink prompt="Favorite Color" value-binding="color">
+///     What's your favorite color?
+/// </TextFieldLink>
+/// ```
+///
+/// ## Attributes
+/// * ``prompt``
+///
+/// ## See Also
+/// * [LiveView Native Live Form](https://github.com/liveview-native/liveview-native-live-form)
+@available(watchOS 9.0, *)
 struct TextFieldLink<R: RootRegistry>: View {
     @ObservedElement var element: ElementNode
     let context: LiveContext<R>
     @FormState var value: String?
+    
+    /// Describes the reason for requesting text input.
+    @Attribute("prompt", transform: { $0?.value.flatMap(SwiftUI.Text.init) })
+    private var prompt: SwiftUI.Text?
     
     init(context: LiveContext<R>) {
         self.context = context
     }
     
     var body: some View {
+#if os(watchOS)
         SwiftUI.TextFieldLink(
-            prompt: element.attributeValue(for: "prompt").flatMap(SwiftUI.Text.init)
+            prompt: prompt
         ) {
             context.buildChildren(of: element)
         } onSubmit: { newValue in
             value = newValue
         }
+#endif
     }
 }
-#endif
