91db54ff53574de3f39ba40891e7a8ef22ec3640
Carson Katri <Carson.katri@gmail.com>
2023-03-16 14:16:26 -0400

    Document PasteButton

--- Sources/LiveViewNative/Views/Controls and Indicators/Buttons/PasteButton.swift
+++ Sources/LiveViewNative/Views/Controls and Indicators/Buttons/PasteButton.swift
@@ -7,9 +7,42 @@
 #if os(iOS) || os(macOS)
 import SwiftUI
 
+/// Sends an event with the clipboard's contents when tapped.
+///
+/// Use the `phx-click` attribute to specify which event to fire on tap.
+///
+/// ```html
+/// <PasteButton phx-click="my_event" />
+/// ```
+///
+/// ```elixir
+/// def handle_event("my_event", %{ "value" => value }, socket) do
+///   {:noreply, assign(socket, :pasted_value, hd(value))}
+/// end
+/// ```
+///
+/// > ``PasteButton`` only sends copied `String` types.
+///
+/// ## Events
+/// * ``click``
 struct PasteButton<R: RootRegistry>: View {
     @ObservedElement private var element
     private let context: LiveContext<R>
+    
+    /// Event sent when tapped.
+    ///
+    /// Sends a payload with the following format:
+    ///
+    /// ```json
+    /// {
+    ///     "value": [
+    ///         "clipboard string 1",
+    ///         "clipboard string 2",
+    ///         ...
+    ///     ]
+    /// }
+    /// ```
+    @Event("phx-click", type: "click") private var click
     
     init(context: LiveContext<R>) {
         self.context = context
@@ -17,12 +50,9 @@ struct PasteButton<R: RootRegistry>: View {
     
     var body: some View {
         SwiftUI.PasteButton(payloadType: String.self) { strings in
-            guard let clickEvent = element.attributeValue(for: "phx-click")
-            else { return }
-            Task {
-                try await context.coordinator.pushEvent(type: "click", event: clickEvent, value: ["value": strings])
-            }
+            click(["value": strings]) {}
         }
+            .preference(key: ProvidedBindingsKey.self, value: ["phx-click"])
     }
 }
 #endif
