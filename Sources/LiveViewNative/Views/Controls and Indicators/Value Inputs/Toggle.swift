a563f06f788a4ce6429dd95aad6d79641d0272ce
Carson Katri <Carson.katri@gmail.com>
2023-03-16 16:49:44 -0400

    Document Toggle

--- Sources/LiveViewNative/Views/Controls and Indicators/Value Inputs/Toggle.swift
+++ Sources/LiveViewNative/Views/Controls and Indicators/Value Inputs/Toggle.swift
@@ -7,12 +7,33 @@
 
 import SwiftUI
 
+/// A form element that controls a boolean value.
+///
+/// Add elements within the toggle to provide a label.
+///
+/// ```html
+/// <Toggle value-binding="is_on">
+///     Lights On
+/// </Toggle>
+/// ```
+///
+/// > Booleans in Elixir are atoms, so bindings can be declared with
+/// > ```elixir
+/// > native_binding :is_on, Atom, false
+/// > ```
+///
+/// ## Attributes
+/// * ``style``
+///
+/// ## See Also
+/// * [LiveView Native Live Form](https://github.com/liveview-native/liveview-native-live-form)
 struct Toggle<R: RootRegistry>: View {
     @ObservedElement private var element: ElementNode
     let context: LiveContext<R>
     
     @FormState(default: false) var value: Bool
     
+    /// The style to apply to this toggle.
     @Attribute("toggle-style") private var style: ToggleStyle = .automatic
     
     init(element: ElementNode, context: LiveContext<R>) {
