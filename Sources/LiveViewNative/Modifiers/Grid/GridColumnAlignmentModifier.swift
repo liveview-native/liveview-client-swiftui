81b54e2fbddf07f84163e81933da6679a9f92b09
Carson Katri <Carson.katri@gmail.com>
2023-03-23 10:12:53 -0400

    Document Grid and related modifiers

--- Sources/LiveViewNative/Modifiers/Grid/GridColumnAlignmentModifier.swift
+++ Sources/LiveViewNative/Modifiers/Grid/GridColumnAlignmentModifier.swift
@@ -7,7 +7,43 @@
 
 import SwiftUI
 
+/// Overrides the alignment of all cells in a column.
+///
+/// Apply this modifier to any cell in a column to modify the horizontal alignment of all cells within the column.
+///
+/// ```html
+/// <Grid>
+///     <GridRow>
+///         <Text>Name</Text>
+///         <Text>Color</Text>
+///     </GridRow>
+///     <GridRow>
+///         <Text>Ocean Blue</Text>
+///         <Rectangle
+///             fill-color="system-blue"
+///             modifiers={@native |> grid_column_alignment(guide: :trailing)}
+///         />
+///     </GridRow>
+/// </Grid>
+/// ```
+///
+/// With the modifier on the rectangle, the "Color" text and the rectangle will both be aligned to the trailing edge of the column.
+///
+/// ## Arguments
+/// * ``guide``
+#if swift(>=5.8)
+@_documentation(visibility: public)
+#endif
 struct GridColumnAlignmentModifier: ViewModifier, Decodable, Equatable {
+    /// The horizontal alignment of cells in the column.
+    ///
+    /// Possible values:
+    /// * `leading`
+    /// * `center`
+    /// * `trailing`
+    #if swift(>=5.8)
+    @_documentation(visibility: public)
+    #endif
     private let guide: HorizontalAlignment
     
     func body(content: Content) -> some View {
--- Sources/LiveViewNative/Views/Layout Containers/Grids/GridRow.swift
+++ Sources/LiveViewNative/Views/Layout Containers/Grids/GridRow.swift
@@ -7,10 +7,40 @@
 
 import SwiftUI
 
+/// A row of items in a grid.
+///
+/// Use with ``Grid`` to define the rows of the grid.
+///
+/// ```html
+/// <Grid>
+///     <GridRow>
+///         <Text>Title</Text>
+///         <Text>Description</Text>
+///     </GridRow>
+///     <GridRow>
+///         <Text>Item #1</Text>
+///         <Text>The first of many items.</Text>
+///     </GridRow>
+/// </Grid>
+/// ```
+///
+/// ## Attributes
+/// * ``alignment``
+///
+/// ## See Also
+/// ### Creating Grids
+/// * ``Grid``
+#if swift(>=5.8)
+@_documentation(visibility: public)
+#endif
 struct GridRow<R: RootRegistry>: View {
     @ObservedElement private var element: ElementNode
     @LiveContext<R> private var context
     
+    /// The positioning of elements within the row.
+    #if swift(>=5.8)
+    @_documentation(visibility: public)
+    #endif
     @Attribute("alignment") private var alignment: VerticalAlignment?
 
     public var body: some View {
