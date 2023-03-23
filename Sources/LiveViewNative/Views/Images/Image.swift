51420237303e1cde561c01f13cb8ced37f89b8c5
Shadowfacts <me@shadowfacts.net>
2023-03-23 13:33:58 -0400

    Document Image
    Closes #338

--- Sources/LiveViewNative/Views/Images/Image.swift
+++ Sources/LiveViewNative/Views/Images/Image.swift
@@ -7,6 +7,26 @@
 
 import SwiftUI
 
+/// Displays an image.
+///
+/// There are two possible sources for images: system-provided images (SF Symbols) or the app's asset catalog.
+///
+/// ### SF Symbols
+/// The platform provides a wide variety of symbol images which are specified with the `system-name` attribute.
+/// See [Apple's documentation](https://developer.apple.com/sf-symbols/) for more information.
+///
+/// The [`symbol-color`](doc:Image/symbolColor) and [`symbol-scale`](doc:Image/symbolScale) attributes may also be provided.
+/// ```html
+/// <Image system-name="cloud.sun.rain" symbol-color="#ff0000" symbol-scale="large" />
+/// ```
+/// ### Asset Catalog
+/// Specify the `name` attribute to use a named image from the app's asset catalog.
+/// ```html
+/// <Image name="MyCustomImage" />
+/// ```
+#if swift(>=5.8)
+@_documentation(visibility: public)
+#endif
 struct Image: View {
     @ObservedElement private var observedElement: ElementNode
     private let overrideElement: ElementNode?
@@ -44,6 +64,14 @@ struct Image: View {
         }
     }
     
+    /// The foreground color of the symbol.
+    ///
+    /// If no color is provided, the symbol will use a color appropriate for the environment.
+    ///
+    /// See ``LiveViewNative/SwiftUI/Color/init(fromNamedOrCSSHex:)``.
+    #if swift(>=5.8)
+    @_documentation(visibility: public)
+    #endif
     private var symbolColor: SwiftUI.Color? {
         if let attr = element.attributeValue(for: "symbol-color") {
             return SwiftUI.Color(fromNamedOrCSSHex: attr)
@@ -52,6 +80,15 @@ struct Image: View {
         }
     }
     
+    /// The display scale of the symbol.
+    ///
+    /// Possible values:
+    /// - `small`
+    /// - `medium`
+    /// - `large`
+    #if swift(>=5.8)
+    @_documentation(visibility: public)
+    #endif
     private var symbolScale: SwiftUI.Image.Scale? {
         switch element.attributeValue(for: "symbol-scale") {
         case nil:
