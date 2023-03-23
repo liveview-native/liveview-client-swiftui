483040c4089ff51cb91b8036eac75b5977d59965
Shadowfacts <me@shadowfacts.net>
2023-03-23 14:57:32 -0400

    Document stacks

--- Sources/LiveViewNative/Views/Layout Containers/Stacks/HStack.swift
+++ Sources/LiveViewNative/Views/Layout Containers/Stacks/HStack.swift
@@ -7,11 +7,37 @@
 
 import SwiftUI
 
+/// Container that lays out its children in a horizontal line.
+///
+/// ```html
+/// <HStack>
+///     <Text>Leading</Text>
+///     <Spacer />
+///     <Text>Trailing</Text>
+/// </HStack>
+/// ```
+///
+/// ## Attributes
+/// - ``alignment``
+/// - ``spacing``
+#if swift(>=5.8)
+@_documentation(visibility: public)
+#endif
 struct HStack<R: RootRegistry>: View {
     @ObservedElement private var element: ElementNode
     @LiveContext<R> private var context
     
+    /// The vertical alignment of views within the stack. Defaults to center-aligned.
+    ///
+    /// See ``LiveViewNative/SwiftUI/VerticalAlignment``.
+    #if swift(>=5.8)
+    @_documentation(visibility: public)
+    #endif
     @Attribute("alignment") private var alignment: VerticalAlignment = .center
+    /// The spacing between views in the stack. If not provided, the stack uses the system spacing.
+    #if swift(>=5.8)
+    @_documentation(visibility: public)
+    #endif
     @Attribute("spacing") private var spacing: Double?
     
     public var body: some View {
--- Sources/LiveViewNative/Views/Layout Containers/Stacks/ZStack.swift
+++ Sources/LiveViewNative/Views/Layout Containers/Stacks/ZStack.swift
@@ -7,10 +7,30 @@
 
 import SwiftUI
 
+/// A container that lays out elements on top of each other, back to front.
+///
+/// ```html
+/// <ZStack>
+///     <Text>Back</Text>
+///     <Text>Front</Text>
+/// </ZStack>
+/// ```
+///
+/// ## Attributes
+/// - ``alignment``
+#if swift(>=5.8)
+@_documentation(visibility: public)
+#endif
 struct ZStack<R: RootRegistry>: View {
     @ObservedElement private var element: ElementNode
     @LiveContext<R> private var context
     
+    /// The alignment in both axes of views within the stack. Defaults to center-aligned.
+    ///
+    /// See ``LiveViewNative/SwiftUI/Alignment``.
+    #if swift(>=5.8)
+    @_documentation(visibility: public)
+    #endif
     @Attribute("alignment") private var alignment: Alignment = .center
     
     public var body: some View {
