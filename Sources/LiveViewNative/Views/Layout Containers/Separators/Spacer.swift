2f4d4e9f996db2b073515c4b7281e066a9e6228d
Shadowfacts <me@shadowfacts.net>
2023-03-23 15:02:35 -0400

    Document Spacer

--- Sources/LiveViewNative/Views/Layout Containers/Separators/Spacer.swift
+++ Sources/LiveViewNative/Views/Layout Containers/Separators/Spacer.swift
@@ -7,7 +7,29 @@
 
 import SwiftUI
 
+/// A view that expands to provide space between other views.
+///
+/// ```html
+/// <VStack>
+///     <Text>First</Text>
+///     <Spacer min-length="50" />
+///     <Text>Second</Text>
+/// </VStack>
+/// ```
+///
+/// Within horizontal and vertical stacks, the spacer only grows along the stack's axis.
+/// Otherwise, the spacer grows in both axes.
+///
+/// ## Attributes
+/// - ``minLength``
+#if swift(>=5.8)
+@_documentation(visibility: public)
+#endif
 struct Spacer: View {
+    /// The minimum size of the spacer. If not provided, the minimum length is the system spacing.
+    #if swift(>=5.8)
+    @_documentation(visibility: public)
+    #endif
     @Attribute("min-length") private var minLength: Double?
     
     public var body: some View {
