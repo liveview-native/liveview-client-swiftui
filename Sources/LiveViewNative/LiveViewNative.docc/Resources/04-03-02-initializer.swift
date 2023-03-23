eea816aa125df76665ee81079982d86b076b34b2
Carson Katri <Carson.katri@gmail.com>
2023-03-23 11:13:09 -0400

    Simplify tutorial

--- Sources/LiveViewNative/LiveViewNative.docc/Resources/04-03-02-initializer.swift
+++ Sources/LiveViewNative/LiveViewNative.docc/Resources/04-03-02-initializer.swift
@@ -2,7 +2,7 @@ import SwiftUI
 import LiveViewNative
 
 struct CatRatingView: View {
-    @Attribute("score", transform: { Int($0?.value ?? "") ?? 0 }) private var score: Int
+    @Attribute("score") private var score: Int
     @LiveContext<MyRegistry> private var context
     
     var body: some View {
