1504f9c37ff27994d1940a094462d28621a35cfd
Carson Katri <Carson.katri@gmail.com>
2023-03-23 15:23:54 -0400

    Document Group

--- Sources/LiveViewNative/Views/Layout Containers/Groups/Group.swift
+++ Sources/LiveViewNative/Views/Layout Containers/Groups/Group.swift
@@ -7,6 +7,37 @@
 
 import SwiftUI
 
+/// A collection of elements.
+///
+/// You can use groups to apply modifiers to multiple elements without code duplication.
+///
+/// ```html
+/// <Group modifiers={@native |> tint(color: :orange)}>
+///     <Button>Orange Button</Button>
+///     <Toggle>Orange Toggle</Toggle>
+///     <Slider>Orange Slider</Slider>
+/// </Group>
+/// ```
+///
+/// Groups can also be used to surpass the 10 child limit.
+///
+/// ```html
+/// <VStack>
+///     <Group>
+///         <Text>1</Text>
+///         <Text>2</Text>
+///         <Text>3</Text>
+///         <Text>4</Text>
+///         <Text>5</Text>
+///         <Text>6</Text>
+///         <Text>7</Text>
+///         <Text>8</Text>
+///         <Text>9</Text>
+///         <Text>10</Text>
+///     </Group>
+///     <Text>11</Text>
+/// </VStack>
+/// ```
 struct Group<R: RootRegistry>: View {
     @ObservedElement private var element: ElementNode
     @LiveContext<R> private var context
