931a824c85f17fc2e85924fde82b1d3d23cee6ad
Carson Katri <Carson.katri@gmail.com>
2023-03-23 16:28:59 -0400

    Document Lazy Grids

--- Sources/LiveViewNative/Utils/PinnedScrollableViews.swift
+++ Sources/LiveViewNative/Utils/PinnedScrollableViews.swift
@@ -8,6 +8,40 @@
 import SwiftUI
 import LiveViewNativeCore
 
+/// Configuration used to pin section headers/footers in lazy containers.
+///
+/// Possible values:
+/// * `section-headers`
+/// * `section-footers`
+/// * `all`
+///
+/// In this example, the current section header will always be visible on the leading edge of the screen, up until the next section is scrolled past the left edge.
+///
+/// ```html
+/// <ScrollView axes="horizontal">
+///     <LazyHGrid
+///         rows={...}
+///         pinned-views="section-headers"
+///     >
+///         <Section>
+///             <Section:header>1-50</Section:header>
+///             <Section:content>
+///                 <%= for i <- 1..50 do %>
+///                     <Text id={i |> Integer.to_string}><%= i %></Text>
+///                 <% end %>
+///             </Section:content>
+///         </Section>
+///         <Section>
+///             <Section:header>51-100</Section:header>
+///             <Section:content>
+///                 <%= for i <- 51..100 do %>
+///                     <Text id={i |> Integer.to_string}><%= i %></Text>
+///                 <% end %>
+///             </Section:content>
+///         </Section>
+///     </LazyHGrid>
+/// </ScrollView>
+/// ```
 extension PinnedScrollableViews: Decodable, AttributeDecodable {
     public init(from decoder: Decoder) throws {
         let container = try decoder.singleValueContainer()
--- Sources/LiveViewNative/Views/Layout Containers/Lazy Grids/LazyHGrid.swift
+++ Sources/LiveViewNative/Views/Layout Containers/Lazy Grids/LazyHGrid.swift
@@ -7,10 +7,49 @@
 
 import SwiftUI
 
+/// Grid that grows horizontally.
+///
+/// Use the ``rows`` attribute to configure the presentation of the grid.
+///
+/// ```html
+/// <LazyHGrid
+///     rows={[
+///         LiveViewNativeSwiftUi.Types.GridItem.fixed(100),
+///         LiveViewNativeSwiftUi.Types.GridItem.flexible(),
+///         LiveViewNativeSwiftUi.Types.GridItem.adaptive(50),
+///     ] |> Jason.encode!}
+/// >
+///     <%= for i <- 1..50 do %>
+///         <Text id={i |> Integer.to_string}><%= i %></Text>
+///     <% end %>
+/// </LazyHGrid>
+/// ```
+///
+/// - Precondition: The ``rows`` attribute must be JSON encoded.
+///
+/// There are 3 types of grid item:
+/// * `fixed(size, spacing, alignment)`: creates a single row that takes up the specified amount of space.
+/// * `flexible(minimum, maximum, spacing, alignment)`: creates a single row that fills the available space.
+/// * `adaptive(minimum, maximum, spacing, alignment)`: fills the available space with as many rows as will fit.
+///
+/// ## Attributes
+/// * ``rows``
+/// * ``alignment``
+/// * ``spacing``
+/// * ``pinnedViews``
+#if swift(>=5.8)
+@_documentation(visibility: public)
+#endif
 struct LazyHGrid<R: RootRegistry>: View {
     @ObservedElement private var element: ElementNode
     @LiveContext<R> private var context
     
+    /// Configured rows to fill with the child elements.
+    ///
+    /// - Precondition: The value of the attribute must be JSON encoded.
+    #if swift(>=5.8)
+    @_documentation(visibility: public)
+    #endif
     @Attribute(
         "rows",
         transform: {
@@ -18,8 +57,22 @@ struct LazyHGrid<R: RootRegistry>: View {
             return try JSONDecoder().decode([GridItem].self, from: value)
         }
     ) private var rows: [GridItem]
+    /// The alignment between rows.
+    #if swift(>=5.8)
+    @_documentation(visibility: public)
+    #endif
     @Attribute("alignment") private var alignment: VerticalAlignment = .center
+    /// The spacing between rows.
+    #if swift(>=5.8)
+    @_documentation(visibility: public)
+    #endif
     @Attribute("spacing") private var spacing: Double?
+    /// Pins section headers/footers.
+    ///
+    /// See ``LiveViewNative/SwiftUI/PinnedScrollableViews``.
+    #if swift(>=5.8)
+    @_documentation(visibility: public)
+    #endif
     @Attribute("pinned-views") private var pinnedViews: PinnedScrollableViews = []
 
     public var body: some View {
