99a64534d271c398bed606e10f814e525061eda6
Carson Katri <Carson.katri@gmail.com>
2023-03-23 16:28:59 -0400

    Document Lazy Grids

--- Sources/LiveViewNative/Views/Layout Containers/Lazy Grids/LazyVGrid.swift
+++ Sources/LiveViewNative/Views/Layout Containers/Lazy Grids/LazyVGrid.swift
@@ -7,10 +7,49 @@
 
 import SwiftUI
 
+/// Grid that grows vertically.
+///
+/// Use the ``columns`` attribute to configure the presentation of the grid.
+///
+/// ```html
+/// <LazyVGrid
+///     columns={[
+///         LiveViewNativeSwiftUi.Types.GridItem.fixed(100),
+///         LiveViewNativeSwiftUi.Types.GridItem.flexible(),
+///         LiveViewNativeSwiftUi.Types.GridItem.adaptive(50),
+///     ] |> Jason.encode!}
+/// >
+///     <%= for i <- 1..50 do %>
+///         <Text id={i |> Integer.to_string}><%= i %></Text>
+///     <% end %>
+/// </LazyVGrid>
+/// ```
+///
+/// - Precondition: The ``columns`` attribute must be JSON encoded.
+///
+/// There are 3 types of grid item:
+/// * `fixed(size, spacing, alignment)`: creates a single column that takes up the specified amount of space.
+/// * `flexible(minimum, maximum, spacing, alignment)`: creates a single column that fills the available space.
+/// * `adaptive(minimum, maximum, spacing, alignment)`: fills the available space with as many columns as will fit.
+///
+/// ## Attributes
+/// * ``columns``
+/// * ``alignment``
+/// * ``spacing``
+/// * ``pinnedViews``
+#if swift(>=5.8)
+@_documentation(visibility: public)
+#endif
 struct LazyVGrid<R: RootRegistry>: View {
     @ObservedElement private var element: ElementNode
     @LiveContext<R> private var context
     
+    /// Configured columns to fill with the child elements.
+    ///
+    /// - Precondition: The value of the attribute must be JSON encoded.
+    #if swift(>=5.8)
+    @_documentation(visibility: public)
+    #endif
     @Attribute(
         "columns",
         transform: {
@@ -18,8 +57,22 @@ struct LazyVGrid<R: RootRegistry>: View {
             return try JSONDecoder().decode([GridItem].self, from: value)
         }
     ) private var columns: [GridItem]
+    /// The alignment between columns.
+    #if swift(>=5.8)
+    @_documentation(visibility: public)
+    #endif
     @Attribute("alignment") private var alignment: HorizontalAlignment = .center
+    /// The spacing between rows.
+    #if swift(>=5.8)
+    @_documentation(visibility: public)
+    #endif
     @Attribute("spacing") private var spacing: Double?
+    /// Pins section headers/footers.
+    ///
+    /// See ``LiveViewNative/SwiftUI/PinnedScrollableViews``.
+    #if swift(>=5.8)
+    @_documentation(visibility: public)
+    #endif
     @Attribute("pinned-views") private var pinnedViews: PinnedScrollableViews = []
     
     public var body: some View {
