9836b0f6102d3c4257ab0831086172d55aeb64b0
Carson Katri <Carson.katri@gmail.com>
2023-03-28 14:50:44 -0400

    Document List

--- Sources/LiveViewNative/Views/Layout Containers/Collection Containers/List.swift
+++ Sources/LiveViewNative/Views/Layout Containers/Collection Containers/List.swift
@@ -7,6 +7,92 @@
 
 import SwiftUI
 
+/// Presents rows of elements.
+///
+/// Each element inside the list is given its own row.
+///
+/// - Precondition: Each child element must have a unique `id` attribute.
+///
+/// ```html
+/// <List>
+///     <%= for sport <- @sports do %>
+///         <Text id={sport.id}><%= sport.name %></Text>
+///     <% end %>
+/// </List>
+/// ```
+///
+/// ### Edit Mode
+/// Use an <doc:EditButton> to enter edit mode. This will allow rows to be moved, selected, and deleted.
+///
+/// ```html
+/// <EditButton />
+/// <List> ... </List>
+/// ```
+///
+/// ### Selecting Rows
+/// Use the ``selection`` binding to synchronize the selected row(s) with the LiveView.
+///
+/// ```html
+/// <List selection="selected_sports">
+///     ...
+/// </List>
+/// ```
+///
+/// ```elixir
+/// defmodule MyAppWeb.SportsLive do
+///   native_binding :selected_sports, List, []
+/// end
+/// ```
+///
+/// ### Deleting and Moving Rows
+/// Set an event name for the ``delete`` attribute to enable the system delete action.
+///
+/// An event is sent with the `index` of the item to delete.
+///
+/// ```html
+/// <List phx-delete="on_delete">
+///     ...
+/// </List>
+/// ```
+///
+/// ```elixir
+/// defmodule MyAppWeb.SportsLive do
+///     def handle_event("on_delete", %{ "index" => index }, socket) do
+///         {:noreply, assign(socket, :items, List.delete_at(socket.assigns.items, index))}
+///     end
+/// end
+/// ```
+///
+/// Use the ``move`` event to enable the system move actions.
+///
+/// An event is sent with the `index` of the item to move and its `destination` index.
+///
+/// ```html
+/// <List phx-move="on_move">
+///     ...
+/// </List>
+/// ```
+///
+/// ```elixir
+/// defmodule MyAppWeb.SportsLive do
+///     def handle_event("on_move", %{ "index" => index, "destination" => destination }, socket) do
+///         {element, list} = List.pop_at(socket.assigns.sports, index)
+///         moved = List.insert_at(list, (if destination > index, do: destination - 1, else: destination), element)
+///         {:noreply, assign(socket, :sports, moved)}
+///     end
+/// end
+/// ```
+///
+/// ## Attributes
+/// * ``selection``
+/// * ``style``
+///
+/// ## Events
+/// * ``delete``
+/// * ``move``
+#if swift(>=5.8)
+@_documentation(visibility: public)
+#endif
 struct List<R: RootRegistry>: View {
     @ObservedElement private var element: ElementNode
     @LiveContext<R> private var context
@@ -14,11 +100,79 @@ struct List<R: RootRegistry>: View {
     @Environment(\.editMode) var editMode
     #endif
     
+    /// Event sent when a row is deleted.
+    ///
+    /// An event is sent with the `index` of the item to delete.
+    ///
+    /// ```html
+    /// <List phx-delete="on_delete">
+    ///     ...
+    /// </List>
+    /// ```
+    ///
+    /// ```elixir
+    /// defmodule MyAppWeb.SportsLive do
+    ///     def handle_event("on_delete", %{ "index" => index }, socket) do
+    ///         {:noreply, assign(socket, :items, List.delete_at(socket.assigns.items, index))}
+    ///     end
+    /// end
+    /// ```
+    #if swift(>=5.8)
+    @_documentation(visibility: public)
+    #endif
     @Event("phx-delete", type: "click") private var delete
+    /// Event sent when a row is moved.
+    ///
+    /// An event is sent with the `index` of the item to move and its `destination` index.
+    ///
+    /// ```html
+    /// <List phx-move="on_move">
+    ///     ...
+    /// </List>
+    /// ```
+    ///
+    /// ```elixir
+    /// defmodule MyAppWeb.SportsLive do
+    ///     def handle_event("on_move", %{ "index" => index, "destination" => destination }, socket) do
+    ///         {element, list} = List.pop_at(socket.assigns.sports, index)
+    ///         moved = List.insert_at(list, (if destination > index, do: destination - 1, else: destination), element)
+    ///         {:noreply, assign(socket, :sports, moved)}
+    ///     end
+    /// end
+    /// ```
+    #if swift(>=5.8)
+    @_documentation(visibility: public)
+    #endif
     @Event("phx-move", type: "click") private var move
     
+    /// Synchronizes the selected rows with the server.
+    ///
+    /// To allow an arbitrary number of rows to be selected, use the `List` type for the binding.
+    /// Use an empty list as the default value to start with no selection.
+    ///
+    /// ```elixir
+    /// defmodule MyAppWeb.SportsLive do
+    ///   native_binding :selected_sports, List, []
+    /// end
+    /// ```
+    ///
+    /// To only allow a single selection, use the `String` type for the binding.
+    /// Use `nil` as the default value to start with no selection.
+    ///
+    /// ```elixir
+    /// defmodule MyAppWeb.SportsLive do
+    ///   native_binding :selected_sport, String, nil
+    /// end
+    /// ```
+    #if swift(>=5.8)
+    @_documentation(visibility: public)
+    #endif
     @LiveBinding(attribute: "selection") private var selection = Selection.single(nil)
     
+    /// The style to apply to this list.
+    #if swift(>=5.8)
+    @_documentation(visibility: public)
+    #endif
     @Attribute("list-style") private var style: ListStyle = .automatic
     
     public var body: some View {
