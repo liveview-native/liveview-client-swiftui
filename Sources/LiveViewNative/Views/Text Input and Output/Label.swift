8f25e5bc2e8c1641250864e110cf3e48c5f53bdf
Carson Katri <Carson.katri@gmail.com>
2023-03-28 10:36:21 -0400

    Document Label

--- Sources/LiveViewNative/Views/Text Input and Output/Label.swift
+++ Sources/LiveViewNative/Views/Text Input and Output/Label.swift
@@ -7,11 +7,52 @@
 
 import SwiftUI
 
+/// A title and icon pair.
+///
+/// Use the `title` and `icon` children to create a label.
+///
+/// ```html
+/// <Label>
+///     <Label:title>
+///         <Text>John Doe</Text>
+///     </Label:title>
+///     <Label:icon>
+///         <Image system-name="person.crop.circle.fill" />
+///     </Label:icon>
+/// </Label>
+/// ```
+///
+/// When using a symbol as the icon, use the ``systemImage`` attribute.
+///
+/// ```html
+/// <Label system-image="person.crop.circle.fill">
+///     <Text>John Doe</Text>
+/// </Label>
+/// ```
+///
+/// ## Attributes
+/// * ``systemImage``
+/// * ``style``
+#if swift(>=5.8)
+@_documentation(visibility: public)
+#endif
 struct Label<R: RootRegistry>: View {
     @ObservedElement private var element: ElementNode
     @LiveContext<R> private var context
     
+    /// A symbol to use for the `icon`.
+    ///
+    /// This attribute takes precedence over the `icon` child.
+    ///
+    /// This is equivalent to the `system-name` attribute on ``Image``.
+    #if swift(>=5.8)
+    @_documentation(visibility: public)
+    #endif
     @Attribute("system-image") private var systemImage: String?
+    /// The style to use for this label.
+    #if swift(>=5.8)
+    @_documentation(visibility: public)
+    #endif
     @Attribute("label-style") private var style: LabelStyle = .automatic
     
     public var body: some View {
