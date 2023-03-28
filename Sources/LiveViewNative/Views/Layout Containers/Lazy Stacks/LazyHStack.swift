fe249d75a0c00978b2b9cd09f44c90dfe460edfd
Carson Katri <Carson.katri@gmail.com>
2023-03-28 09:57:44 -0400

    Document Lazy*Stack

--- Sources/LiveViewNative/Views/Layout Containers/Lazy Stacks/LazyHStack.swift
+++ Sources/LiveViewNative/Views/Layout Containers/Lazy Stacks/LazyHStack.swift
@@ -7,12 +7,55 @@
 
 import SwiftUI
 
+/// Horizontal stack that creates Views lazily.
+///
+/// Lazy stacks behave similar to their non-lazy counterparts.
+///
+/// Use the ``pinnedViews`` attribute to pin section headers/footers when contained in a `ScrollView`.
+///
+/// ```html
+/// <ScrollView axes="horizontal">
+///     <LazyHStack>
+///         <%= for i <- 1..50 do %>
+///             <Text id={i |> Integer.to_string} font="largeTitle"><%= i %></Text>
+///         <% end %>
+///     </LazyHStack>
+/// </ScrollView>
+/// ```
+///
+/// ## Attributes
+/// * ``alignment``
+/// * ``spacing``
+/// * ``pinnedViews``
+///
+/// ## See Also
+/// ### Stacks
+/// * ``HStack``
+#if swift(>=5.8)
+@_documentation(visibility: public)
+#endif
 struct LazyHStack<R: RootRegistry>: View {
     @ObservedElement private var element: ElementNode
     @LiveContext<R> private var context
     
+    /// The vertical alignment of views within the stack. Defaults to center-aligned.
+    ///
+    /// See ``LiveViewNative/SwiftUI/VerticalAlignment``.
+    #if swift(>=5.8)
+    @_documentation(visibility: public)
+    #endif
     @Attribute("alignment") private var alignment: VerticalAlignment = .center
+    /// The spacing between views in the stack. If not provided, the stack uses the system spacing.
+    #if swift(>=5.8)
+    @_documentation(visibility: public)
+    #endif
     @Attribute("spacing") private var spacing: Double?
+    /// Pins section headers/footers.
+    ///
+    /// See ``LiveViewNative/SwiftUI/PinnedScrollableViews``.
+    #if swift(>=5.8)
+    @_documentation(visibility: public)
+    #endif
     @Attribute("pinned-views") private var pinnedViews: PinnedScrollableViews = []
     
     public var body: some View {
