819b23cf402ed3960539ed9ea4e852e730e8294e
Shadowfacts <me@shadowfacts.net>
2023-03-29 17:01:29 -0400

    Document NavigationLink
    Closes #349

--- Sources/LiveViewNative/Views/Layout Containers/Presentation Containers/NavigationLink.swift
+++ Sources/LiveViewNative/Views/Layout Containers/Presentation Containers/NavigationLink.swift
@@ -8,14 +8,36 @@
 import SwiftUI
 import Combine
 
+/// A control users can tap to navigate to another live view.
+///
+/// This only has an effect if the ``LiveSessionConfiguration`` was configured with navigation enabled.
+///
+/// ```html
+/// <NavigationLink destination={"/products/#{@product.id}"}>
+///     <Text>More Information</Text>
+/// </NavigationLink>
+/// ```
+///
+/// ## Attributes
+/// - ``destination``
+/// - ``disabled``
+#if swift(>=5.8)
+@_documentation(visibility: public)
+#endif
 @available(iOS 16.0, *)
 struct NavigationLink<R: RootRegistry>: View {
     @ObservedElement private var element: ElementNode
     @LiveContext<R> private var context
-    @EnvironmentObject private var navCoordinator: NavigationCoordinator<R>
-    @State private var doNavigationCancellable: AnyCancellable?
     
+    /// The URL of the destination live view, relative to the current live view's URL.
+    #if swift(>=5.8)
+    @_documentation(visibility: public)
+    #endif
     @Attribute("destination") private var destination: String
+    /// Whether the link is disabled.
+    #if swift(>=5.8)
+    @_documentation(visibility: public)
+    #endif
     @Attribute("disabled") private var disabled: Bool
     
     @ViewBuilder
