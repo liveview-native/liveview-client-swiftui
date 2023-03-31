f09282a0358c9b6ea8d5f330488f32ff20d8d4a3
Shadowfacts <me@shadowfacts.net>
2023-03-31 13:50:31 -0400

    Document Picker and TagModifier
    Closes #364

--- Sources/LiveViewNative/Modifiers/View Fundamentals Modifiers/TagModifier.swift
+++ Sources/LiveViewNative/Modifiers/View Fundamentals Modifiers/TagModifier.swift
@@ -7,6 +7,14 @@
 
 import SwiftUI
 
+/// Specifies the tag value of a view, for use with ``Picker``.
+///
+/// ```html
+/// <Text modifiers={tag(@native, tag: "a")}>Option A</Text>
+/// ```
+#if swift(>=5.8)
+@_documentation(visibility: public)
+#endif
 struct TagModifier: ViewModifier, Decodable {
     private let value: String?
     
