314ad31a1c23fc5752aacff60c3d86f8982c9788
May Matyi <may@matyi.net>
2023-04-01 15:21:11 -0700

    Add defaults

--- lib/live_view_native_swift_ui/modifiers/offset.ex
+++ lib/live_view_native_swift_ui/modifiers/offset.ex
@@ -2,7 +2,7 @@ defmodule LiveViewNativeSwiftUi.Modifiers.Offset do
   use LiveViewNativePlatform.Modifier
 
   modifier_schema "offset" do
-    field :x, :float
-    field :y, :float
+    field :x, :float, default: 0.0
+    field :y, :float, default: 0.0
   end
 end
