bdeef5ac6d836fae6291a91bc19f660beca52702
Carson Katri <Carson.katri@gmail.com>
2023-04-03 09:54:55 -0400

    Add to schema

--- lib/live_view_native_swift_ui/modifiers/bold.ex
+++ lib/live_view_native_swift_ui/modifiers/bold.ex
@@ -2,5 +2,6 @@ defmodule LiveViewNativeSwiftUi.Modifiers.Bold do
   use LiveViewNativePlatform.Modifier
 
   modifier_schema "bold" do
+    field :active, :boolean
   end
 end
--- lib/live_view_native_swift_ui/modifiers/italic.ex
+++ lib/live_view_native_swift_ui/modifiers/italic.ex
@@ -2,5 +2,6 @@ defmodule LiveViewNativeSwiftUi.Modifiers.Italic do
   use LiveViewNativePlatform.Modifier
 
   modifier_schema "italic" do
+    field :active, :boolean
   end
 end
