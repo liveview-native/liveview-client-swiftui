1f95f52641afad4792b6f17d7534e980751d0de3
Syed <syed.murtza@dockyard.com>
2023-04-14 19:48:58 +0500

    Update router.ex files

--- Sources/LiveViewNative/LiveViewNative.docc/Resources/01-01-04-router01.ex
+++ Sources/LiveViewNative/LiveViewNative.docc/Resources/01-01-04-router01.ex
@@ -18,6 +18,7 @@ defmodule LvnTutorialWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
+    live "/cats", CatsListLive
   end
 
   # Other scopes may use custom stacks.
