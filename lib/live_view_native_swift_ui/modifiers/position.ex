e61937c03aeafb97e5de94604ec575962081cf5f
May Matyi <may@matyi.net>
2023-04-14 13:30:31 -0700

    Fix type inconsistency

--- lib/live_view_native_swift_ui/modifiers/position.ex
+++ lib/live_view_native_swift_ui/modifiers/position.ex
@@ -2,7 +2,7 @@ defmodule LiveViewNativeSwiftUi.Modifiers.Position do
   use LiveViewNativePlatform.Modifier
 
   modifier_schema "position" do
-    field :x, :float, default: 0
-    field :y, :float, default: 0
+    field :x, :float, default: 0.0
+    field :y, :float, default: 0.0
   end
 end
