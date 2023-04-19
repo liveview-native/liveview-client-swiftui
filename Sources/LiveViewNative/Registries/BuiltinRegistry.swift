14824d3b9f52d47cdce2b19c735eddfff73b894d
Shadowfacts <me@shadowfacts.net>
2023-04-19 15:43:09 -0400

    Workaround for exceeding stack limit on iOS devices (#809)
    Fixes #806

--- Sources/LiveViewNative/Registries/BuiltinRegistry.swift
+++ Sources/LiveViewNative/Registries/BuiltinRegistry.swift
@@ -17,158 +17,8 @@ protocol BuiltinRegistryProtocol {
 }
 
 struct BuiltinRegistry<R: RootRegistry>: BuiltinRegistryProtocol {
-    // note: the context parameter is unused, but it needs to be there for swift to infer the generic type R
-    @ViewBuilder
     static func lookup(_ name: String, _ element: ElementNode) -> some View {
-        switch name {
-        case "AsyncImage":
-            AsyncImage<R>()
-        case "Button":
-            Button<R>(action: nil)
-        case "Capsule":
-            Shape(shape: Capsule(from: element))
-        case "Circle":
-            Shape(shape: Circle())
-        case "Color":
-            Color()
-#if os(iOS) || os(macOS)
-        case "ColorPicker":
-            ColorPicker<R>()
-#endif
-        case "ContainerRelativeShape":
-            Shape(shape: ContainerRelativeShape())
-#if os(iOS) || os(macOS)
-        case "ControlGroup":
-            ControlGroup<R>()
-#endif
-#if os(iOS) || os(macOS)
-        case "DatePicker":
-            DatePicker<R>()
-#endif
-#if os(iOS) || os(macOS)
-        case "DisclosureGroup":
-            DisclosureGroup<R>()
-#endif
-        case "Divider":
-            Divider()
-#if os(iOS)
-        case "EditButton":
-            EditButton()
-#endif
-        case "Ellipse":
-            Shape(shape: Ellipse())
-        case "Form":
-            Form<R>()
-#if !os(tvOS)
-        case "Gauge":
-            Gauge<R>()
-#endif
-        case "Group":
-            Group<R>()
-        case "Grid":
-            Grid<R>()
-        case "GridRow":
-            GridRow<R>()
-#if os(iOS) || os(macOS)
-        case "GroupBox":
-            GroupBox<R>()
-#endif
-#if os(macOS)
-        case "HSplitView":
-            HSplitView<R>()
-#endif
-        case "HStack":
-            HStack<R>()
-        case "Image":
-            Image()
-        case "Label":
-            Label<R>()
-        case "LabeledContent":
-            LabeledContent<R>()
-        case "LazyHGrid":
-            LazyHGrid<R>()
-        case "LazyHStack":
-            LazyHStack<R>()
-        case "LazyVGrid":
-            LazyVGrid<R>()
-        case "LazyVStack":
-            LazyVStack<R>()
-        case "Link":
-            Link<R>()
-        case "List":
-            List<R>()
-#if !os(watchOS)
-        case "Menu":
-            Menu<R>()
-#endif
-#if os(iOS)
-        case "MultiDatePicker":
-            MultiDatePicker<R>()
-#endif
-        case "NamespaceContext":
-            NamespaceContext<R>()
-        case "NavigationLink":
-            NavigationLink<R>()
-#if os(iOS) || os(macOS)
-        case "PasteButton":
-            PasteButton<R>()
-#endif
-        case "ProgressView":
-            ProgressView<R>()
-        case "Picker":
-            Picker<R>()
-        case "Rectangle":
-            Shape(shape: Rectangle())
-        case "RenameButton":
-            RenameButton()
-        case "RoundedRectangle":
-            Shape(shape: RoundedRectangle(from: element))
-        case "ScrollView":
-            ScrollView<R>()
-        case "Section":
-            Section<R>()
-        case "SecureField":
-            SecureField<R>()
-        case "ShareLink":
-            ShareLink<R>()
-        case "Slider":
-            Slider<R>()
-        case "Stepper":
-            Stepper<R>()
-        case "Spacer":
-            Spacer()
-#if os(iOS) || os(macOS)
-        case "Table":
-            Table<R>()
-#endif
-        case "Text":
-            Text<R>()
-#if os(iOS) || os(macOS)
-        case "TextEditor":
-            TextEditor()
-#endif
-        case "TextField":
-            TextField<R>()
-#if os(watchOS)
-        case "TextFieldLink":
-            TextFieldLink<R>()
-#endif
-        case "Toggle":
-            Toggle<R>()
-#if os(macOS)
-        case "VSplitView":
-            VSplitView<R>()
-#endif
-        case "VStack":
-            VStack<R>()
-        case "ViewThatFits":
-            ViewThatFits<R>()
-        case "ZStack":
-            ZStack<R>()
-        default:
-            // log here that view type cannot be found
-            EmptyView()
-        }
+        return BuiltinElement<R>(name: name, element: element)
     }
 
     enum ModifierType: String {
@@ -339,6 +189,166 @@ struct BuiltinRegistry<R: RootRegistry>: BuiltinRegistryProtocol {
             try TrackingModifier(from: decoder)
         case .transition:
             try TransitionModifier<R>(from: decoder)
+        }
+    }
+}
+
+// This switch can't be inlined into BuiltinRegistry.lookup because it results in that method's return type
+// being a massive pile of nested _ConditionalContents. Instead, lift it out into a separate View type
+// that lookup can return, so it doesn't blow up the stack.
+// See #806 for more details.
+struct BuiltinElement<R: RootRegistry>: View {
+    let name: String
+    let element: ElementNode
+    var body: some View {
+        switch name {
+        case "AsyncImage":
+            AsyncImage<R>()
+        case "Button":
+            Button<R>(action: nil)
+        case "Capsule":
+            Shape(shape: Capsule(from: element))
+        case "Circle":
+            Shape(shape: Circle())
+        case "Color":
+            Color()
+#if os(iOS) || os(macOS)
+        case "ColorPicker":
+            ColorPicker<R>()
+#endif
+        case "ContainerRelativeShape":
+            Shape(shape: ContainerRelativeShape())
+#if os(iOS) || os(macOS)
+        case "ControlGroup":
+            ControlGroup<R>()
+#endif
+#if os(iOS) || os(macOS)
+        case "DatePicker":
+            DatePicker<R>()
+#endif
+#if os(iOS) || os(macOS)
+        case "DisclosureGroup":
+            DisclosureGroup<R>()
+#endif
+        case "Divider":
+            Divider()
+#if os(iOS)
+        case "EditButton":
+            EditButton()
+#endif
+        case "Ellipse":
+            Shape(shape: Ellipse())
+        case "Form":
+            Form<R>()
+#if !os(tvOS)
+        case "Gauge":
+            Gauge<R>()
+#endif
+        case "Group":
+            Group<R>()
+        case "Grid":
+            Grid<R>()
+        case "GridRow":
+            GridRow<R>()
+#if os(iOS) || os(macOS)
+        case "GroupBox":
+            GroupBox<R>()
+#endif
+#if os(macOS)
+        case "HSplitView":
+            HSplitView<R>()
+#endif
+        case "HStack":
+            HStack<R>()
+        case "Image":
+            Image()
+        case "Label":
+            Label<R>()
+        case "LabeledContent":
+            LabeledContent<R>()
+        case "LazyHGrid":
+            LazyHGrid<R>()
+        case "LazyHStack":
+            LazyHStack<R>()
+        case "LazyVGrid":
+            LazyVGrid<R>()
+        case "LazyVStack":
+            LazyVStack<R>()
+        case "Link":
+            Link<R>()
+        case "List":
+            List<R>()
+#if !os(watchOS)
+        case "Menu":
+            Menu<R>()
+#endif
+#if os(iOS)
+        case "MultiDatePicker":
+            MultiDatePicker<R>()
+#endif
+        case "NamespaceContext":
+            NamespaceContext<R>()
+        case "NavigationLink":
+            NavigationLink<R>()
+#if os(iOS) || os(macOS)
+        case "PasteButton":
+            PasteButton<R>()
+#endif
+        case "ProgressView":
+            ProgressView<R>()
+        case "Picker":
+            Picker<R>()
+        case "Rectangle":
+            Shape(shape: Rectangle())
+        case "RenameButton":
+            RenameButton()
+        case "RoundedRectangle":
+            Shape(shape: RoundedRectangle(from: element))
+        case "ScrollView":
+            ScrollView<R>()
+        case "Section":
+            Section<R>()
+        case "SecureField":
+            SecureField<R>()
+        case "ShareLink":
+            ShareLink<R>()
+        case "Slider":
+            Slider<R>()
+        case "Stepper":
+            Stepper<R>()
+        case "Spacer":
+            Spacer()
+#if os(iOS) || os(macOS)
+        case "Table":
+            Table<R>()
+#endif
+        case "Text":
+            Text<R>()
+#if os(iOS) || os(macOS)
+        case "TextEditor":
+            TextEditor()
+#endif
+        case "TextField":
+            TextField<R>()
+#if os(watchOS)
+        case "TextFieldLink":
+            TextFieldLink<R>()
+#endif
+        case "Toggle":
+            Toggle<R>()
+#if os(macOS)
+        case "VSplitView":
+            VSplitView<R>()
+#endif
+        case "VStack":
+            VStack<R>()
+        case "ViewThatFits":
+            ViewThatFits<R>()
+        case "ZStack":
+            ZStack<R>()
+        default:
+            // log here that view type cannot be found
+            EmptyView()
         }
     }
 }
