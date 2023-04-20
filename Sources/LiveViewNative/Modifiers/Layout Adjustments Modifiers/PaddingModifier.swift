9ed40f991dc311e847aaed606f6977d629f7b3aa
Carson Katri <Carson.katri@gmail.com>
2023-04-20 14:46:35 -0400

    Refactor and document padding modifier

--- Sources/LiveViewNative/Modifiers/Layout Adjustments Modifiers/PaddingModifier.swift
+++ Sources/LiveViewNative/Modifiers/Layout Adjustments Modifiers/PaddingModifier.swift
@@ -7,19 +7,85 @@
 
 import SwiftUI
 
+/// Adds space around the element.
+///
+/// Pass ``LiveViewNative/SwiftUI/EdgeInsets`` to the ``insets`` argument to set different padding amounts for each edge.
+///
+/// ```html
+/// <Text modifiers={padding(@native, insets: [top: 10, bottom: 20])}>
+///     ...
+/// </Text>
+/// ```
+///
+/// Set a ``length`` value to inset a set of ``edges`` by the same amount.
+///
+/// ```html
+/// <Text modifiers={padding(@native, length: 10)}>
+///     ...
+/// </Text>
+/// <Text modifiers={padding(@native, edges: :horizontal, length: 10)}>
+///     ...
+/// </Text>
+/// ```
+///
+/// To use the system padding amount, omit the ``length`` argument.
+///
+/// ```html
+/// <Text modifiers={padding(@native)}>
+///     ...
+/// </Text>
+/// ```
+///
+/// ## Arguments
+/// * ``insets``
+/// * ``edges``
+/// * ``length``
+#if swift(>=5.8)
+@_documentation(visibility: public)
+#endif
 struct PaddingModifier: ViewModifier, Decodable, Equatable {
-    private let insets: EdgeInsets
+    /// The amount to inset by.
+    ///
+    /// This argument takes precedence over other options.
+    ///
+    /// See ``LiveViewNative/SwiftUI/EdgeInsets`` for more details on creating insets.
+    #if swift(>=5.8)
+    @_documentation(visibility: public)
+    #endif
+    private let insets: EdgeInsets?
+    
+    /// The edges to inset. Defaults to `all`.
+    ///
+    /// See ``LiveViewNative/SwiftUI/Edge`` for more details on creating insets.
+#if swift(>=5.8)
+    @_documentation(visibility: public)
+#endif
+    private let edges: Edge.Set
+    
+    /// The amount to inset all ``edges`` by.
+    #if swift(>=5.8)
+    @_documentation(visibility: public)
+    #endif
+    private let length: CGFloat?
     
     init(from decoder: Decoder) throws {
-        let container = try decoder.singleValueContainer()
-        self.insets = try container.decode(EdgeInsets.self)
-    }
-    
-    init(insets: EdgeInsets) {
-        self.insets = insets
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.insets = try container.decodeIfPresent(EdgeInsets.self, forKey: .insets)
+        self.edges = try container.decodeIfPresent(Edge.Set.self, forKey: .edges) ?? .all
+        self.length = try container.decodeIfPresent(CGFloat.self, forKey: .length)
     }
     
     func body(content: Content) -> some View {
-        content.padding(insets)
+        if let insets {
+            content.padding(insets)
+        } else {
+            content.padding(edges, length)
+        }
+    }
+    
+    enum CodingKeys: CodingKey {
+        case insets
+        case edges
+        case length
     }
 }
