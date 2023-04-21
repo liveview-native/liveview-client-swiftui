87c204d318a53498f57759e9d8a48f156a01ddc5
Dylan Ginsburg <dylan@hey.com>
2023-04-21 09:32:08 -0400

    Add `color_invert` modifier

---
+++ Sources/LiveViewNative/Modifiers/Drawing and Graphics Modifiers/ColorInvertModifier.swift
@@ -0,0 +1,22 @@
+//
+//  ColorInvertModifier.swift
+//  LiveViewNative
+//
+//  Created by Dylan.Ginsburg on 4/20/23.
+//
+
+import SwiftUI
+
+/// Inverts the colors in this view.
+///
+/// ```html
+/// <Circle modifiers={@native |> foreground_style(primary: {:color, :red}) |> color_invert()} />
+/// ```
+#if swift(>=5.8)
+@_documentation(visibility: public)
+#endif
+struct ColorInvertModifier: ViewModifier, Decodable {
+    func body(content: Content) -> some View {
+        content.colorInvert()
+    }
+}
