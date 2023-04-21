59ef50e771e090c97784cc0db5b066c174497a10
Dylan Ginsburg <dylan@hey.com>
2023-04-21 14:08:57 -0400

    Fix mismatch between Elixir and Swift values (#842)

--- lib/live_view_native_swift_ui/modifiers/text_field_style.ex
+++ lib/live_view_native_swift_ui/modifiers/text_field_style.ex
@@ -2,6 +2,6 @@ defmodule LiveViewNativeSwiftUi.Modifiers.TextFieldStyle do
   use LiveViewNativePlatform.Modifier
 
   modifier_schema "text_field_style" do
-    field :style, Ecto.Enum, values: ~w(automatic plain rounded-border square-border)a
+    field :style, Ecto.Enum, values: ~w(automatic plain rounded_border square_border)a
   end
 end
