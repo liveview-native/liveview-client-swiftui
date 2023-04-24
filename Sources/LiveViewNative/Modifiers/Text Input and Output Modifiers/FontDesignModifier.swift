0c4a30bfe9c743aadc7a22c8d39f642a99c7ac46
May Matyi <may@matyi.net>
2023-04-24 11:33:42 -0700

    Add fontDesign modifier

---
+++ Sources/LiveViewNative/Modifiers/Text Input and Output Modifiers/FontDesignModifier.swift
@@ -0,0 +1,54 @@
+//
+//  FontDesignModifier.swift
+// LiveViewNative
+//
+//  Created by May Matyi on 4/24/23.
+//
+
+import SwiftUI
+
+/// Applies a font design to any element.
+///
+/// <Text modifiers={font_design(@native, design: :default)}>This text is default</Text>
+/// <Text modifiers={font_design(@native, design: :monospaced)}>This text is monospaced</Text>
+/// <Text modifiers={font_design(@native, design: :rounded)}>This text is rounded</Text>
+/// <Text modifiers={font_design(@native, design: :serif)}>This text is serif</Text>
+///
+/// ## Arguments
+/// * ``design``
+#if swift(>=5.8)
+@_documentation(visibility: public)
+#endif
+struct FontDesignModifier: ViewModifier, Decodable {
+    /// The font design to apply to the view.
+    #if swift(>=5.8)
+    @_documentation(visibility: public)
+    #endif
+    private var design: Font.Design
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let designString = try container.decode(String.self, forKey: .design)
+
+        switch designString {
+        case "default":
+            design = .default
+        case "monospaced":
+            design = .monospaced
+        case "rounded":
+            design = .rounded
+        case "serif":
+            design = .serif
+        default:
+            throw DecodingError.dataCorruptedError(forKey: .design, in: container, debugDescription: "expected valid value for Font.Design");
+        }
+    }
+
+    func body(content: Content) -> some View {
+        content.font(.system(size: UIFont.labelFontSize, design: design))
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case design
+    }
+}
