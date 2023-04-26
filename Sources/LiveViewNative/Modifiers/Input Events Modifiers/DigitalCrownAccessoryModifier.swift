af5be8f1d0ca612b1c3e884ce34278cb4bfd391f
Dylan Ginsburg <dylan@hey.com>
2023-04-26 16:40:16 -0400

    Add digital_crown_accessory content modifier

---
+++ Sources/LiveViewNative/Modifiers/Input Events Modifiers/DigitalCrownAccessoryModifier.swift
@@ -0,0 +1,56 @@
+//
+//  DigitalCrownAccessoryModifier.swift
+//  LiveViewNative
+//
+//  Created by Dylan.Ginsburg on 4/26/23.
+//
+
+import SwiftUI
+
+/// Places an accessory View next to the Digital Crown on Apple Watch.
+///
+/// Nested content is referenced by its namespace using the `content` argument.
+///
+/// ```html
+/// <List modifiers={@native |> digital_crown_accessory(content: :dca_view)}>
+///     ...
+///     <digital_crown_accessory:dca_view>
+///         <Image system-name="heart.fill" modifiers={@native |> digital_crown_accessory_visibility(visibility: :visible)} />
+///     </digital_crown_accessory:dca_view>
+/// </List>
+/// ```
+///
+/// ## Arguments
+/// * ``content``
+#if swift(>=5.8)
+@_documentation(visibility: public)
+#endif
+@available(watchOS 9.0, *)
+struct DigitalCrownAccessoryModifier<R: RootRegistry>: ViewModifier, Decodable {
+    @ObservedElement private var element
+    @LiveContext<R> private var context
+    
+    /// A reference to the element that contains the accessory view.
+    #if swift(>=5.8)
+    @_documentation(visibility: public)
+    #endif
+    let content: String
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.content = try container.decode(String.self, forKey: .content)
+    }
+
+    func body(content: Content) -> some View {
+        content
+            #if os(watchOS)
+            .digitalCrownAccessory {
+                context.buildChildren(of: element, withTagName: self.content, namespace: "digital_crown_accessory")
+            }
+            #endif
+    }
+
+    enum CodingKeys: CodingKey {
+        case content
+    }
+}
--- Sources/LiveViewNative/Modifiers/Input Events Modifiers/DigitalCrownAccessoryVisibilityModifier.swift
+++ Sources/LiveViewNative/Modifiers/Input Events Modifiers/DigitalCrownAccessoryVisibilityModifier.swift
@@ -9,7 +9,15 @@ import SwiftUI
 
 /// Specifies the visibility of Digital Crown accessory Views on Apple Watch.
 ///
+/// Use this method to customize the visibility of a Digital Crown accessory View created with the ``digital_crown_accessory`` modifier.
+///
 /// ```html
+/// <List modifiers={@native |> digital_crown_accessory(content: :dca_view)}>
+///     ...
+///     <digital_crown_accessory:dca_view>
+///         <Image system-name="heart.fill" modifiers={@native |> digital_crown_accessory_visibility(visibility: :visible)} />
+///     </digital_crown_accessory:dca_view>
+/// </List>
 /// ```
 ///
 /// ## Arguments
