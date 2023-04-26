5ce247e737d165eaefe75bdaf5832cc097f2ab71
Carson Katri <Carson.katri@gmail.com>
2023-04-26 12:53:51 -0400

    Update lib/live_view_native_swift_ui/types/shape_style/shadow_style.ex
    Co-authored-by: May Matyi <may@matyi.net>
    Signed-off-by: Carson Katri <Carson.katri@gmail.com>

--- lib/live_view_native_swift_ui/types/shape_style/shadow_style.ex
+++ lib/live_view_native_swift_ui/types/shape_style/shadow_style.ex
@@ -14,16 +14,22 @@ defmodule LiveViewNativeSwiftUi.Types.ShadowStyle do
   alias LiveViewNativeSwiftUi.Types.Color
 
   def cast({type, opts}) when type in [:drop, :inner] and is_map(opts) or is_list(opts) do
-    with_type = opts
+    with_type =
+      opts
       |> Enum.into(%{})
       |> Map.put(:type, type)
-    {:ok, struct(
-      __MODULE__,
-      (if Map.has_key?(with_type, :color),
-        do: Map.replace(with_type, :color, elem(Color.cast(with_type[:color]), 1)),
-        else: with_type
-      )
-    )}
+
+    {:ok, struct(__MODULE__, cast_color(with_type))}
   end
+
   def cast(_), do: :error
-end
+
+  ###
+
+  defp cast_color(%{color: color} = with_type) do
+    cast_color = Color.cast(color)
+
+    Map.replace(with_type, :color, elem(cast_color, 1))
+  end
+
+  defp cast_color(with_type), do: with_type
