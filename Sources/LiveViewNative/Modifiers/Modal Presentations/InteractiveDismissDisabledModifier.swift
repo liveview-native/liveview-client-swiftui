7812ab0cd921a02106886db98d6666cb99a418bc
Shadowfacts <me@shadowfacts.net>
2023-04-28 14:48:17 -0400

    Add interactiveDismissDisabled modifier
    Closes #652

---
+++ Sources/LiveViewNative/Modifiers/Modal Presentations/InteractiveDismissDisabledModifier.swift
@@ -0,0 +1,42 @@
+//
+//  InteractiveDismissDisabledModifier.swift
+//  LiveViewNative
+//
+//  Created by Shadowfacts on 4/28/23.
+//
+
+import SwiftUI
+
+/// A modifier that disables interactive dismissal of the sheet it contains.
+///
+/// Use this modifier in the content of a ``SheetModifier``.
+///
+/// ```html
+/// <Button phx-click="toggle" modifiers={sheet(@native, content: :content, on_dismiss: "dismiss", is_presented: :show)}>
+///   Present Sheet
+///
+///   <sheet:content>
+///     <VStack modifiers={interactive_dismiss_disabled(@native)}>
+///       <Text>Hello, world!</Text>
+///       <Button phx-click="toggle">Dismiss</Button>
+///     </VStack>
+///   </sheet:content>
+/// </Button>
+/// ```
+///
+/// ## Attributes
+/// - ``disabled``
+#if swift(>=5.8)
+@_documentation(visibility: public)
+#endif
+struct InteractiveDismissDisabledModifier: ViewModifier, Decodable {
+    /// Whether interactive dismiss is disabled. Defaults to true.
+    #if swift(>=5.8)
+    @_documentation(visibility: public)
+    #endif
+    private let disabled: Bool
+    
+    func body(content: Content) -> some View {
+        content.interactiveDismissDisabled(disabled)
+    }
+}
