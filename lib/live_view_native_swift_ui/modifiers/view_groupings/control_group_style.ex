4f7133a88cff348309171cdc30ce6766a0df5438
Carson Katri <Carson.katri@gmail.com>
2023-05-02 16:22:22 -0400

    Add missing modifier

--- lib/live_view_native_swift_ui/modifiers/view_groupings/control_group_style.ex
+++ lib/live_view_native_swift_ui/modifiers/view_groupings/control_group_style.ex
@@ -2,6 +2,6 @@ defmodule LiveViewNativeSwiftUi.Modifiers.ControlGroupStyle do
   use LiveViewNativePlatform.Modifier
 
   modifier_schema "control_group_style" do
-    field :style, Ecto.Enum, values: ~w(automatic navigation)
+    field :style, Ecto.Enum, values: ~w(automatic navigation)a
   end
 end
