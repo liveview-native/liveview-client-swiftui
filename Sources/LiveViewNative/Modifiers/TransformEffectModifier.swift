cf849834c40e735daf1a608252da85f28263e1b2
Carson Katri <Carson.katri@gmail.com>
2023-05-17 16:37:50 -0400

    Add `transform_effect` modifier (#935)
    * Add `transform_effect` modifier
    
    * Add tests, and fix concat ordering

---
+++ Sources/LiveViewNative/Modifiers/TransformEffectModifier.swift
@@ -0,0 +1,44 @@
+//
+//  TransformEffectModifier.swift
+//  LiveViewNative
+//
+//  Created by Carson Katri on 5/16/2023.
+//
+
+import SwiftUI
+
+/// Transforms an element with a ``LiveViewNative/CoreFoundation/CGAffineTransform``.
+///
+/// Provide a ``LiveViewNative/CoreFoundation/CGAffineTransform`` to the ``transform`` argument to modify the element.
+///
+/// - Note: The frame of the element is not affected.
+///
+/// ```html
+/// <Text modifiers={transform_effect(@native, transform: [
+///     {:translate, {50, 0}},
+///     {:rotate, {:degrees, 45}},
+///     {:scale, 0.5}
+/// ])}>
+///     Hello, world!
+/// </Text>
+/// ```
+///
+/// ## Arguments
+/// * ``transform``
+#if swift(>=5.8)
+@_documentation(visibility: public)
+#endif
+struct TransformEffectModifier: ViewModifier, Decodable {
+    /// The transformation to apply.
+    ///
+    /// See ``LiveViewNative/CoreFoundation/CGAffineTransform`` for more details.
+    #if swift(>=5.8)
+    @_documentation(visibility: public)
+    #endif
+    private let transform: CGAffineTransform
+
+    func body(content: Content) -> some View {
+        content.transformEffect(transform)
+    }
+}
+
