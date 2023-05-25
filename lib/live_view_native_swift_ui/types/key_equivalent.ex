ad9ba959a7ea623f6cd977bc1e75862c7e0326c4
Syed <syed.murtza@dockyard.com>
2023-05-25 19:27:53 +0500

    Use byte_size/1 instead of String.length/1 in function gaurd clause

--- lib/live_view_native_swift_ui/types/key_equivalent.ex
+++ lib/live_view_native_swift_ui/types/key_equivalent.ex
@@ -20,7 +20,7 @@ defmodule LiveViewNativeSwiftUi.Types.KeyEquivalent do
   ]
 
   def cast(value) when is_atom(value) and value in @keys, do: {:ok, Atom.to_string(value)}
-  def cast(value) when is_binary(value) and String.length(value) == 1, do: {:ok, value}
+  def cast(value) when is_binary(value) and byte_size(value) == 1, do: {:ok, value}
   def cast(value) when is_list(value) and length(value) == 1, do: {:ok, to_string(value)}
   def cast(_), do: :error
 end
