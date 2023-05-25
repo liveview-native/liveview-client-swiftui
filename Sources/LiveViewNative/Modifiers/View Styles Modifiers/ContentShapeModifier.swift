20d54f73f46a725e67eccabb0e477d2873f5ffa4
Syed <syed.murtza@dockyard.com>
2023-05-25 19:00:54 +0500

    Add ContentShapeModifier

---
+++ Sources/LiveViewNative/Modifiers/View Styles Modifiers/ContentShapeModifier.swift
@@ -0,0 +1,99 @@
+//
+//  ContentShapeModifier.swift
+//  
+//
+//  Created by murtza on 24/05/2023.
+//
+
+import SwiftUI
+
+/// Sets the content shape for this view.
+///
+/// ```html
+/// <HStack modifiers={content_shape(@native, kind: :hover_effect, shape: :rectangle)}>
+///   <Image system-name="heart.circle"></Image>
+/// </HStack>
+///
+/// <HStack modifiers={content_shape(@native, kind: :hover_effect, shape: :rectangle, eo_fill: true)}>
+///   <Image system-name="heart.circle"></Image>
+/// </HStack>
+///
+/// <HStack modifiers={content_shape(@native, kind: [:hover_effect, :interaction], shape: :rectangle, eo_fill: true)}>
+///   <Image system-name="heart.circle"></Image>
+/// </HStack>
+/// ```
+///
+/// ## Arguments
+/// * ``kind``
+/// * ``shape``
+/// * ``eo_fill``
+#if swift(>=5.8)
+@_documentation(visibility: public)
+#endif
+struct ContentShapeModifier: ViewModifier, Decodable {
+    /// The kinds to apply to this content shape.
+    #if swift(>=5.8)
+    @_documentation(visibility: public)
+    #endif
+    private let kind: ContentShapeKinds
+    
+    /// The shape to use.
+    #if swift(>=5.8)
+    @_documentation(visibility: public)
+    #endif
+    private var shape: ShapeKind
+    
+    /// A Boolean that indicates whether the shape is interpreted with the even-odd winding number rule.
+    #if swift(>=5.8)
+    @_documentation(visibility: public)
+    #endif
+    private var eoFill: Bool = false
+
+    func body(content: Content) -> some View {
+        switch shape {
+        case .capsule:
+            content.contentShape(kind, Capsule(), eoFill: eoFill)
+        case .circle:
+            content.contentShape(kind, Circle(), eoFill: eoFill)
+        case .containerRelativeShape:
+            content.contentShape(kind, ContainerRelativeShape(), eoFill: eoFill)
+        case .ellipse:
+            content.contentShape(kind, Ellipse(), eoFill: eoFill)
+        case .rectangle:
+            content.contentShape(kind, Rectangle(), eoFill: eoFill)
+        }
+        
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case kind
+        case shape
+    }
+}
+
+/// The shape kind to use.
+#if swift(>=5.8)
+@_documentation(visibility: public)
+#endif
+private enum ShapeKind: String, Decodable {
+    #if swift(>=5.8)
+    @_documentation(visibility: public)
+    #endif
+    case capsule
+    #if swift(>=5.8)
+    @_documentation(visibility: public)
+    #endif
+    case circle
+    #if swift(>=5.8)
+    @_documentation(visibility: public)
+    #endif
+    case containerRelativeShape = "container_relative_shape"
+    #if swift(>=5.8)
+    @_documentation(visibility: public)
+    #endif
+    case ellipse
+    #if swift(>=5.8)
+    @_documentation(visibility: public)
+    #endif
+    case rectangle
+}
