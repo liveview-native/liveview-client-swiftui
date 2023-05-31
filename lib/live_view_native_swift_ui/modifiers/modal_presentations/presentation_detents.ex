9c348f2410093557ce986735a4b27fe06ce7f7d9
Carson Katri <Carson.katri@gmail.com>
2023-05-31 15:33:10 -0400

    Add selection argument

--- lib/live_view_native_swift_ui/modifiers/modal_presentations/presentation_detents.ex
+++ lib/live_view_native_swift_ui/modifiers/modal_presentations/presentation_detents.ex
@@ -1,9 +1,10 @@
 defmodule LiveViewNativeSwiftUi.Modifiers.PresentationDetents do
   use LiveViewNativePlatform.Modifier
 
-  alias LiveViewNativeSwiftUi.Types.PresentationDetent
+  alias LiveViewNativeSwiftUi.Types.{PresentationDetent, NativeBindingName}
 
   modifier_schema "presentation_detents" do
-    field(:detents, {:array, PresentationDetent})
+    field :detents, {:array, PresentationDetent}
+    field :selection, NativeBindingName, default: nil
   end
 end
