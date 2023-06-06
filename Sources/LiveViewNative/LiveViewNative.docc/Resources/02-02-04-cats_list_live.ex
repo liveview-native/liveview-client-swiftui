015e9d9fb0bbd9f227ae2d92316574271fbe869c
May Matyi <may@matyi.net>
2023-06-06 13:50:17 -0700

    Change ~SUI sigil to ~SWIFTUI

--- Sources/LiveViewNative/LiveViewNative.docc/Resources/02-02-04-cats_list_live.ex
+++ Sources/LiveViewNative/LiveViewNative.docc/Resources/02-02-04-cats_list_live.ex
@@ -30,7 +30,7 @@ defmodule LvnTutorialWeb.CatsListLive do
   end
 
   def render(%{platform_id: :swiftui} = assigns) do
-    ~SUI"""
+    ~SWIFTUI"""
     <List>
       <%= for {name, favorite} <- @cats_and_favorites do %>
         <HStack id={name}>
