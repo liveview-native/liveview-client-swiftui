23b612e4ddcf4b21418f38a25ccc92e550cd103c
Carson Katri <Carson.katri@gmail.com>
2023-06-08 14:36:45 -0400

    Simplify filter

--- Sources/LiveViewNative/Property Wrappers/LiveContext.swift
+++ Sources/LiveViewNative/Property Wrappers/LiveContext.swift
@@ -138,7 +138,7 @@ public struct LiveContext<R: RootRegistry>: DynamicProperty {
         of element: ElementNode
     ) -> [NodeChildrenSequence.Element] {
         element.children().filter {
-            !($0.asElement()?.attributes.contains(where: { $0.name.rawValue == "template" }) ?? false)
+            !$0.attributes.contains(where: { $0.name.rawValue == "template" })
         }
     }
 }
