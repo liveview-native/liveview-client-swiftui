3abd07e2efbc5c8f185b14b60f542863f235d9b1
May Matyi <may@matyi.net>
2023-06-14 16:08:15 -0700

    Add foreground_style modifier constructor

--- lib/live_view_native_swift_ui/modifiers/drawing_and_graphics/foreground_style.ex
+++ lib/live_view_native_swift_ui/modifiers/drawing_and_graphics/foreground_style.ex
@@ -8,4 +8,10 @@ defmodule LiveViewNativeSwiftUi.Modifiers.ForegroundStyle do
     field :secondary, ShapeStyle
     field :tertiary, ShapeStyle
   end
+
+  def params(%ShapeStyle{} = primary), do: [primary: primary]
+  def params(params) when is_list(params) or is_map(params), do: params
+  def params(primary), do: [primary: primary]
+  def params(primary, secondary), do: [primary: primary, secondary: secondary]
+  def params(primary, secondary, tertiary), do: [primary: primary, secondary: secondary, tertiary: tertiary]
 end
