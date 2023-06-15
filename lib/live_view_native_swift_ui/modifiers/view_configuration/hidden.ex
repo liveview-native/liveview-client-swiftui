11f4f6c188876d65f6a59c75558b1623310cfc9a
May Matyi <may@matyi.net>
2023-06-14 17:05:34 -0700

    Add hidden modifier constructor

--- lib/live_view_native_swift_ui/modifiers/view_configuration/hidden.ex
+++ lib/live_view_native_swift_ui/modifiers/view_configuration/hidden.ex
@@ -4,4 +4,7 @@ defmodule LiveViewNativeSwiftUi.Modifiers.Hidden do
   modifier_schema "hidden" do
     field :is_active, :boolean
   end
+
+  def params(is_active) when is_boolean(is_active), do: [is_active: is_active]
+  def params(params), do: params
 end
