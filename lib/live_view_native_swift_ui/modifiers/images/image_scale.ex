4995a120ba6aee2a092f243efd62c43890de2eb7
May Matyi <may@matyi.net>
2023-06-14 17:08:27 -0700

    Add image_scale modifier constructor

--- lib/live_view_native_swift_ui/modifiers/images/image_scale.ex
+++ lib/live_view_native_swift_ui/modifiers/images/image_scale.ex
@@ -4,4 +4,7 @@ defmodule LiveViewNativeSwiftUi.Modifiers.ImageScale do
   modifier_schema "image_scale" do
     field :scale, Ecto.Enum, values: ~w(small medium large)a
   end
+
+  def params(scale) when is_atom(scale) and not is_boolean(scale) and not is_nil(scale) , do: [scale: scale]
+  def params(params), do: params
 end
