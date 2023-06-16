5e8e85a5074de4b5c0be1015561922fcd168101d
May Matyi <may@matyi.net>
2023-06-15 17:28:47 -0700

    Add z_index modifier constructor

--- lib/live_view_native_swift_ui/modifiers/layout_fundamentals/z_index.ex
+++ lib/live_view_native_swift_ui/modifiers/layout_fundamentals/z_index.ex
@@ -4,4 +4,7 @@ defmodule LiveViewNativeSwiftUi.Modifiers.ZIndex do
   modifier_schema "z_index" do
     field :value, :float
   end
+
+  def params(z_index) when is_float(z_index), do: [z_index: z_index]
+  def params(params), do: params
 end
