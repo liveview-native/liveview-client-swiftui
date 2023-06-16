a68e7f52122fa1d3ead5a9d93ccaa94f089d6e1f
May Matyi <may@matyi.net>
2023-06-15 17:07:11 -0700

    Add monospaced and monospaced_digit modifier constructors

--- lib/live_view_native_swift_ui/modifiers/text_input_and_output/monospaced.ex
+++ lib/live_view_native_swift_ui/modifiers/text_input_and_output/monospaced.ex
@@ -4,4 +4,7 @@ defmodule LiveViewNativeSwiftUi.Modifiers.Monospaced do
   modifier_schema "monospaced" do
     field :is_active, :boolean, default: true
   end
+
+  def params(is_active) when is_boolean(is_active), do: [is_active: is_active]
+  def params(params), do: params
 end
--- lib/live_view_native_swift_ui/modifiers/text_input_and_output/monospaced_digit.ex
+++ lib/live_view_native_swift_ui/modifiers/text_input_and_output/monospaced_digit.ex
@@ -2,4 +2,6 @@ defmodule LiveViewNativeSwiftUi.Modifiers.MonospacedDigit do
   use LiveViewNativePlatform.Modifier
 
   modifier_schema "monospaced_digit"
+
+  def params, do: []
 end
