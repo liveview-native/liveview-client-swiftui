7ffcad6ebe5d680f750f2afe0164c3c801e5ceb6
May Matyi <may@matyi.net>
2023-06-15 17:17:59 -0700

    Add aspect_ratio modifier constructor

--- lib/live_view_native_swift_ui/modifiers/drawing_and_graphics/aspect_ratio.ex
+++ lib/live_view_native_swift_ui/modifiers/drawing_and_graphics/aspect_ratio.ex
@@ -5,4 +5,7 @@ defmodule LiveViewNativeSwiftUi.Modifiers.AspectRatio do
     field :aspect_ratio, {:array, :float}, default: [1.0, 1.0]
     field :content_mode, Ecto.Enum, values: ~w(fill fit)a
   end
+
+  def params(aspect_ratio, params) when is_list(params) or is_map(params), do: [aspect_ratio: aspect_ratio, content_mode: params[:content_mode]]
+  def params(params), do: params
 end
