e0028d87e445badaf575a9550afdede2fcec1569
Carson Katri <Carson.katri@gmail.com>
2023-06-21 18:25:15 -0400

    Add layout adjustment modifiers

--- lib/live_view_native_swift_ui/modifiers/layout_adjustments/scene_padding.ex
+++ lib/live_view_native_swift_ui/modifiers/layout_adjustments/scene_padding.ex
@@ -7,4 +7,8 @@ defmodule LiveViewNativeSwiftUi.Modifiers.ScenePadding do
     field :padding, Ecto.Enum, values: ~w(automatic minimum navigation_bar)a, default: :automatic
     field :edges, EdgeSet
   end
+
+  def params(padding, [edges: edges]), do: [padding: padding, edges: edges]
+  def params(padding) when is_atom(padding), do: [padding: padding]
+  def params(params), do: params
 end
