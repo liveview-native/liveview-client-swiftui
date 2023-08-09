ab4fd9a36570a04f3137561113e647ff2d59e0fc
May Matyi <may@matyi.net>
2023-08-09 14:06:55 -0700

    Updates for live_view_native_platform 0.0.8-rc.0

--- lib/live_view_native_swift_ui/modifiers/drawing_and_graphics/corner_radius.ex
+++ lib/live_view_native_swift_ui/modifiers/drawing_and_graphics/corner_radius.ex
@@ -2,7 +2,7 @@ defmodule LiveViewNativeSwiftUi.Modifiers.CornerRadius do
   use LiveViewNativePlatform.Modifier
 
   modifier_schema "corner_radius" do
-    field :radius, :float
-    field :antialiased, :boolean, default: true
+    field(:radius, :float)
+    field(:antialiased, :boolean, default: true)
   end
 end
