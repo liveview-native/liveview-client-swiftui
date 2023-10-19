e188e9e4ed9b1d5022e97d843222ce002c88d934
Brian Cardarella <bcardarella@gmail.com>
2023-10-18 20:58:44 -0400

    Added event and underscore numbers

--- test/live_view_native/swiftui/rules_helpers_test.exs
+++ test/live_view_native/swiftui/rules_helpers_test.exs
@@ -7,4 +7,11 @@ defmodule LiveViewNative.SwiftUI.RulesHelpersTest do
       assert RulesHelpers.to_ime("foobar") == {:., [], [nil, :foobar]}
     end
   end
+
+  describe "event" do
+    test "convert an event call to an AST node" do
+      assert RulesHelpers.event("change") == {:__event__, [], ["change", []]}
+      assert RulesHelpers.event("change", throttle: 1_000) == {:__event__, [], ["change", [throttle: 1_000]]}
+    end
+  end
 end
