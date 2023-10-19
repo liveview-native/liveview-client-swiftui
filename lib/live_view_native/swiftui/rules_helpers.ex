73574d1879197923735813afdabefba1cfd3f96c
Brian Cardarella <bcardarella@gmail.com>
2023-10-18 20:58:44 -0400

    Added event and underscore numbers

--- lib/live_view_native/swiftui/rules_helpers.ex
+++ lib/live_view_native/swiftui/rules_helpers.ex
@@ -4,4 +4,8 @@ defmodule LiveViewNative.SwiftUI.RulesHelpers do
   def to_ime(expr) when is_binary(expr) do
     {:., [], [nil, String.to_atom(expr)]}
   end
+
+  def event(name, opts \\ []) do
+    {:__event__, [], [name, opts]}
+  end
 end
