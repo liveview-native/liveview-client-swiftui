7df158e5a47198aca107ceaec7d36d46da93627a
Brian Cardarella <bcardarella@gmail.com>
2023-10-24 21:07:23 -0400

    Fixed the to_ime double ast node rendering
    The parser will now convert `to_ime` to call `to_atom` helper

--- lib/live_view_native/swiftui/rules_helpers.ex
+++ lib/live_view_native/swiftui/rules_helpers.ex
@@ -1,11 +1,4 @@
 defmodule LiveViewNative.SwiftUI.RulesHelpers do
   use LiveViewNative.Stylesheet.RulesParser.Helpers, additional: []
 
-  def to_ime(expr) when is_binary(expr) do
-    {:., [], [nil, String.to_atom(expr)]}
-  end
-
-  def event(name, opts \\ []) do
-    {:__event__, [], [name, opts]}
-  end
 end
