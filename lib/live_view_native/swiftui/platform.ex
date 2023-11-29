8533a388be22bdd37183de9ebc521b8eac0818fe
May Matyi <may@matyi.net>
2023-11-28 16:47:55 -0800

    Define default layouts

--- lib/live_view_native/swiftui/platform.ex
+++ lib/live_view_native/swiftui/platform.ex
@@ -11,9 +11,20 @@ defmodule LiveViewNative.SwiftUI.Platform do
   defimpl LiveViewNativePlatform.Kit do
     require Logger
 
+    @default_layouts %{
+      app: "<%= @inner_content %>",
+      root: """
+        <csrf-token value={get_csrf_token()} />
+        <NavigationStack>
+          <%= @inner_content %>
+        </NavigationStack>
+      """
+    }
+
     def compile(struct) do
       LiveViewNativePlatform.Env.define(:swiftui,
         custom_modifiers: struct.custom_modifiers,
+        default_layouts: @default_layouts,
         render_macro: :sigil_SWIFTUI,
         otp_app: :live_view_native_swift_ui,
         valid_targets: ~w(mac pad phone tv vision watch)a
