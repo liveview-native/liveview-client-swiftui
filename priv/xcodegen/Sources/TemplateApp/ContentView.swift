56bf420fcdd2a23d17ffd12c9daaf9cf812e0b4b
Carson Katri <Carson.katri@gmail.com>
2023-12-19 12:34:31 -0500

    Use `supportedDestinations` to create a multiplatform target

--- priv/xcodegen/Sources/TemplateApp/ContentView.swift
+++ priv/xcodegen/Sources/TemplateApp/ContentView.swift
@@ -8,6 +8,9 @@ import LiveViewNative
 
 struct ContentView: View {
     var body: some View {
-        LiveView(.automatic(URL(string: "https://%LVN_PREFERRED_PROD_URL%%LVN_PREFERRED_ROUTE%")!))
+        LiveView(.automatic(
+            development: .localhost(path: "%LVN_PREFERRED_ROUTE%"),
+            production: .custom(URL(string: "https://%LVN_PREFERRED_PROD_URL%%LVN_PREFERRED_ROUTE%")!)
+        ))
     }
 }
