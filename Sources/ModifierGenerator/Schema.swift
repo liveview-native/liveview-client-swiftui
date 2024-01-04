7859c2dfaeebb4dacc43780755e2c0728eb37ddd
Carson Katri <Carson.katri@gmail.com>
2024-01-04 14:19:36 -0500

    Add `--schema` flag to `ModifierGenerator` (#1211)
    * Add `--schema` flag for generating modifier data for the VS Code extension
    
    * Add help text to the flag

---
+++ Sources/ModifierGenerator/Schema.swift
@@ -0,0 +1,34 @@
+//
+//  Schema.swift
+//
+//
+//  Created by Carson Katri on 1/2/24.
+//
+
+import Foundation
+
+/// A JSON representation of the available modifiers.
+/// This structure is used by the LiveView Native VS Code extension to provide completions for modifiers.
+struct Schema: Encodable {
+    var modifiers: [String:[Signature]] = [:]
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encode(modifiers)
+    }
+    
+    struct Signature: Encodable {
+        let parameters: [Parameter]
+        
+        func encode(to encoder: Encoder) throws {
+            var container = encoder.singleValueContainer()
+            try container.encode(parameters)
+        }
+        
+        struct Parameter: Encodable {
+            let firstName: String
+            let secondName: String?
+            let type: String
+        }
+    }
+}
