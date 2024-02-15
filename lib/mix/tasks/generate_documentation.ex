cc37378e010d6bdda792b4952ff2d2ef1fa59d00
Carson Katri <Carson.katri@gmail.com>
2024-02-15 16:21:27 -0500

    Improve `reference` url resolution

--- lib/mix/tasks/generate_documentation.ex
+++ lib/mix/tasks/generate_documentation.ex
@@ -16,39 +16,57 @@ defmodule Mix.Tasks.Lvn.SwiftUi.GenerateDocumentation do
           docs = Jason.decode!(data)
           path = "generated_docs/#{category}/#{view}.md"
           File.mkdir_p!(Path.dirname(path))
-          File.write!(path, markdown(docs))
+          File.write!(path, markdown(docs, docs))
         end
       end
     end
   end
 
-  defp markdown(%{
-    "metadata" => %{ "title" => title },
-    "abstract" => abstract,
-    "primaryContentSections" => content
-  }) do
+  defp markdown(
+    %{
+      "metadata" => %{ "title" => title },
+      "abstract" => abstract,
+      "primaryContentSections" => content
+    },
+    ctx
+  ) do
     """
     # `#{title |> String.replace("<", "&lt;") |> String.replace(">", "&gt;")}`
-    #{markdown(abstract)}
+    #{markdown(abstract, ctx)}
 
-    #{markdown(content)}
+    #{markdown(content, ctx)}
     """
   end
 
-  defp markdown(data) when is_list(data), do: data |> Enum.map(&markdown/1) |> Enum.join()
+  defp markdown(data, ctx) when is_list(data), do: data |> Enum.map(&(markdown(&1, ctx))) |> Enum.join()
 
-  defp markdown(%{ "kind" => "content", "content" => content }), do: markdown(content)
+  defp markdown(%{ "kind" => "content", "content" => content }, ctx), do: markdown(content, ctx)
 
-  defp markdown(%{ "type" => "text", "text" => text }), do: text
-  defp markdown(%{ "type" => "heading", "level" => level, "text" => text }), do: "#{String.duplicate("#", level)} #{text}\n"
-  defp markdown(%{ "type" => "paragraph", "inlineContent" => content }), do: "#{markdown(content)}\n"
-  defp markdown(%{ "type" => "codeListing", "syntax" => "html" } = code), do: markdown(Map.put(code, "syntax", "heex"))
-  defp markdown(%{ "type" => "codeListing", "syntax" => syntax, "code" => code }), do: "\n```#{syntax}\n#{Enum.join(code, "\n")}\n```\n"
-  defp markdown(%{ "type" => "codeVoice", "code" => code }), do: "`#{code}`"
-  defp markdown(%{ "type" => "reference", "identifier" => identifier }), do: "[#{Path.basename(identifier)}](#{identifier})"
-  defp markdown(%{ "type" => "unorderedList", "items" => items }), do: items
-    |> Enum.map(fn %{ "content" => content } -> "* #{markdown(content)}" end)
+  defp markdown(%{ "type" => "text", "text" => text }, _ctx), do: text
+  defp markdown(%{ "type" => "heading", "level" => level, "text" => text }, _ctx), do: "#{String.duplicate("#", level)} #{text}\n"
+  defp markdown(%{ "type" => "paragraph", "inlineContent" => content }, ctx), do: "#{markdown(content, ctx)}\n"
+
+  defp markdown(%{ "type" => "codeListing", "syntax" => "html" } = code, ctx), do: markdown(Map.put(code, "syntax", "heex"), ctx)
+  defp markdown(%{ "type" => "codeListing", "syntax" => syntax, "code" => code }, _ctx), do: "\n```#{syntax}\n#{Enum.join(code, "\n")}\n```\n"
+  defp markdown(%{ "type" => "codeVoice", "code" => code }, _ctx), do: "`#{code}`"
+
+  defp markdown(%{ "type" => "unorderedList", "items" => items }, ctx), do: items
+    |> Enum.map(fn %{ "content" => content } -> "* #{markdown(content, ctx)}" end)
     |> Enum.join()
 
-  defp markdown(_data), do: ""
+  defp markdown(
+    %{ "type" => "reference", "identifier" => identifier },
+    %{ "references" => references }
+  ) do
+    %{ "title" => title, "url" => url } = Map.get(references, identifier)
+    resolved_url = case url do
+      "/documentation/liveviewnative/" <> rest ->
+        "#{rest}.html"
+      url ->
+        url
+    end
+    "[#{title}](#{resolved_url})"
+  end
+
+  defp markdown(_data, _ctx), do: ""
 end
