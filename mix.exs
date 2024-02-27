d061be90fc99395c94f65ccfecf5290baf7455bd
Brooklin Myers <brooklinjazzmyers@gmail.com>
2024-02-27 10:29:20 -0500

    Exdoc gen Review Suggestions (#1276)
    * add tests for generate_documentation and cleanup temp doc folders to resolve ElixirLS issue
    
    * add feature flags and mix task logs
    
    * Add comment about the temporary swiftui doc folder

--- mix.exs
+++ mix.exs
@@ -14,7 +14,8 @@ defmodule LiveViewNative.SwiftUI.MixProject do
       deps: deps(),
       package: package(),
       elixirc_paths: elixirc_paths(Mix.env()),
-      docs: docs()
+      docs: docs(),
+      aliases: aliases()
     ]
   end
 
@@ -24,12 +25,18 @@ defmodule LiveViewNative.SwiftUI.MixProject do
     ]
   end
 
+  defp aliases do
+    [
+      docs: ["lvn.swift_ui.generate_documentation", "docs"]
+    ]
+  end
+
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
   defp deps do
     [
-      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+      {:ex_doc, ">= 0.0.0", only: [:dev, :test], runtime: false},
       {:makeup_swift, "~> 0.0.1"},
       {:makeup_json, "~> 0.1.0"},
       {:makeup_eex, ">= 0.1.1"},
@@ -46,11 +53,26 @@ defmodule LiveViewNative.SwiftUI.MixProject do
   end
 
   defp docs do
-    [
-      extras: ["README.md"] ++ Path.wildcard("generated_docs/**/*.{md,cheatmd}"),
-      groups_for_extras: Path.wildcard("generated_docs/*")
+    # Feature Flagging Docs
+    extras =
+      if System.get_env("INCLUDE_GENERATED_DOCS") do
+        ["README.md"] ++ Path.wildcard("generated_docs/**/*.{md,cheatmd}")
+      else
+        ["README.md"]
+      end
+
+    groups_for_extras =
+      if System.get_env("INCLUDE_GENERATED_DOCS") do
+        Path.wildcard("generated_docs/*")
         |> Enum.map(fn p -> {Path.basename(p), Path.wildcard("#{p}/*.md")} end)
-        |> Map.new,
+        |> Map.new()
+      else
+        []
+      end
+
+    [
+      extras: extras,
+      groups_for_extras: groups_for_extras,
       main: "readme",
       source_url: @source_url,
       source_ref: "v#{@version}"
@@ -65,7 +87,8 @@ defmodule LiveViewNative.SwiftUI.MixProject do
       licenses: ["MIT"],
       links: %{
         "GitHub" => @source_url,
-        "Built by DockYard, Expert Elixir & Phoenix Consultants" => "https://dockyard.com/phoenix-consulting"
+        "Built by DockYard, Expert Elixir & Phoenix Consultants" =>
+          "https://dockyard.com/phoenix-consulting"
       }
     }
   end
