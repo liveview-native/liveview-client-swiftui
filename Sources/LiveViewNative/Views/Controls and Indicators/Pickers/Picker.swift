5eebe5ccb2f82edba5e27e205a518b0cb7820c57
Carson Katri <Carson.katri@gmail.com>
2024-09-03 15:05:13 -0400

    Optimize Picker with plain text options

--- Sources/LiveViewNative/Views/Controls and Indicators/Pickers/Picker.swift
+++ Sources/LiveViewNative/Views/Controls and Indicators/Pickers/Picker.swift
@@ -36,8 +36,24 @@ struct Picker<Root: RootRegistry>: View {
     @FormState("selection") private var selection: String?
     
     var body: some View {
-        SwiftUI.Picker(selection: $selection) {
-            $liveElement.children(in: "content", default: true)
+        SwiftUI.Picker.init(selection: $selection) {
+            ForEach($liveElement.childNodes(in: "content", default: true), id: \.id) { node in
+                if let element = node.asElement() {
+                    // For simple Text elements, we can skip the slow element conversion, and convert directly to Text.
+                    // This will be a common path for apps using `Picker` with large numbers of elements.
+                    if element.tag == "Text",
+                       element.attributes.count == 1,
+                       let attribute = element.attributes.first,
+                       attribute.name.namespace == nil,
+                       attribute.name.name == "tag"
+                    {
+                        SwiftUI.Text(verbatim: element.innerText())
+                            .tag(attribute.value)
+                    } else {
+                        $liveElement.context.buildElement(element)
+                    }
+                }
+            }
         } label: {
             $liveElement.children(in: "label")
         }
