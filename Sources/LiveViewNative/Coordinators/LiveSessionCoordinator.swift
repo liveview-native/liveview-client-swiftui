5be5349dad3ca1bc4fa67d63232715d7b2730eed
Carson Katri <Carson.katri@gmail.com>
2024-09-12 10:49:19 -0400

    Use LiveChannel.document and add LiveViewCoordinator.disconnect

--- Sources/LiveViewNative/Coordinators/LiveSessionCoordinator.swift
+++ Sources/LiveViewNative/Coordinators/LiveSessionCoordinator.swift
@@ -113,10 +113,15 @@ public class LiveSessionCoordinator<R: RootRegistry>: ObservableObject {
                 Task {
                     if prev.count > next.count {
                         // back navigation
-//                        try await next.last!.coordinator.connect(redirect: true)
+                        let liveChannel = try! await self.liveSocket!.joinLiveviewChannel(.some([
+                            "_format": .str(string: Self.platform),
+                            "_interface": Self.platformParams
+                        ]))
+                        try await next.last!.coordinator.join(liveChannel)
                     } else if next.count > prev.count && prev.count > 0 {
+                        print("Forward navigation to \(next.last!.url)")
                         // forward navigation (from `redirect` or `<NavigationLink>`)
-//                        await prev.last?.coordinator.disconnect()
+                        try await prev.last?.coordinator.disconnect()
 //                        try await next.last?.coordinator.connect(redirect: true)
                     }
                 }
@@ -205,9 +210,11 @@ public class LiveSessionCoordinator<R: RootRegistry>: ObservableObject {
     }
 
     private func disconnect(preserveNavigationPath: Bool = false) async {
-        // disconnect all views
-        for entry in navigationPath {
-            await entry.coordinator.disconnect()
+        for entry in self.navigationPath {
+            try! await entry.coordinator.disconnect()
+            if !preserveNavigationPath {
+                entry.coordinator.document = nil
+            }
         }
         // reset all documents if navigation path is being reset.
         if !preserveNavigationPath {
