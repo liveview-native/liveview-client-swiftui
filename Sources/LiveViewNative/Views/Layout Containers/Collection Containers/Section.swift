7f7842f018447f8d31f16f8544bd665c28495709
Carson Katri <Carson.katri@gmail.com>
2024-12-13 14:27:54 -0500

    Add `isExpanded` to Section to replace `collapsible`

--- Sources/LiveViewNative/Views/Layout Containers/Collection Containers/Section.swift
+++ Sources/LiveViewNative/Views/Layout Containers/Collection Containers/Section.swift
@@ -37,16 +37,16 @@ import SwiftUI
 /// </Section>
 /// ```
 ///
-/// On macOS, ``ListStyle/sidebar`` can have collapsible sections. Use the ``collapsible`` attribute to make a section collapsible.
+/// On macOS, ``ListStyle/sidebar`` can have collapsible sections. Use the ``isExpanded`` attribute to make a section collapsible.
 ///
 /// ```html
-/// <Section collapsible>
+/// <Section isExpanded={@is_expanded} phx-change="expansion_changed">
 ///     ...
 /// </Section>
 /// ```
 ///
 /// ## Attributes
-/// * ``collapsible``
+/// * ``isExpanded``
 ///
 /// ## Children
 /// * `content` - The main body of the section.
@@ -55,34 +55,57 @@ import SwiftUI
 @_documentation(visibility: public)
 @LiveElement
 struct Section<Root: RootRegistry>: View {
-    /// Enables this section to be collapsed in sidebar lists on macOS.
+    /// Enables this section to be collapsed in sidebar lists.
     @_documentation(visibility: public)
-    private var collapsible: Bool = false
+    @ChangeTracked(attribute: .init(name: "isExpanded"))
+    private var isExpanded: Bool? = nil
+    
+    var content: some View {
+        let elements = $liveElement.childNodes(in: "content", default: true)
+            .map { (node) -> ForEachElement in
+                if let element = node.asElement(),
+                   let id = element.attributeValue(for: .init(name: "id"))
+                {
+                    return .keyed(node, id: id)
+                } else {
+                    return .unkeyed(node)
+                }
+            }
+        return ForEach(elements) { childNode in
+            ViewTreeBuilder<Root>.NodeView(node: childNode.node, context: $liveElement.context.storage)
+                .trackListItemScrollOffset(id: childNode.id)
+        }
+    }
+    
+    var header: some View {
+        $liveElement.children(in: "header")
+    }
+    
+    var footer: some View {
+        $liveElement.children(in: "footer")
+    }
     
     public var body: some View {
-        SwiftUI.Section {
-            let elements = $liveElement.childNodes(in: "content", default: true)
-                .map { (node) -> ForEachElement in
-                    if let element = node.asElement(),
-                       let id = element.attributeValue(for: .init(name: "id"))
-                    {
-                        return .keyed(node, id: id)
-                    } else {
-                        return .unkeyed(node)
-                    }
-                }
-            ForEach(elements) { childNode in
-                ViewTreeBuilder<Root>.NodeView(node: childNode.node, context: $liveElement.context.storage)
-                    .trackListItemScrollOffset(id: childNode.id)
+        if isExpanded != nil,
+           #available(iOS 17.0, macOS 14.0, tvOS 17.0, watchOS 10.0, visionOS 1.0, *)
+        {
+            SwiftUI.Section(isExpanded: Binding {
+                isExpanded ?? false
+            } set: {
+                isExpanded = $0
+            }) {
+                content
+            } header: {
+                header
             }
-        } header: {
-            $liveElement.children(in: "header")
-        } footer: {
-            $liveElement.children(in: "footer")
+        } else {
+            SwiftUI.Section {
+                content
+            } header: {
+                header
+            } footer: {
+                footer
+            }
         }
-        #if os(macOS)
-            .collapsible(collapsible)
-        #endif
-        
     }
 }
